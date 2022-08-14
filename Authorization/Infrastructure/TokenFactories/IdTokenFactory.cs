96600ff09af2ca728fe03e4aae8f6746edda1eba
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2022-08-14 14:51:06 +0200

    update: idtokenfactory to extend tokenfactory

--- Authorization/Infrastructure/TokenFactories/IdTokenFactory.cs
+++ Authorization/Infrastructure/TokenFactories/IdTokenFactory.cs
@@ -1,5 +1,7 @@
 using Infrastructure.Repositories;
+using Infrastructure.TokenFactories;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -7,72 +9,42 @@ using System.Text.Json;
 
 namespace AuthorizationServer.TokenFactories;
 
-public class IdTokenFactory
+public class IdTokenFactory : TokenFactory
 {
-  private readonly IdentityConfiguration _identityConfiguration;
-  private readonly TokenValidationParameters _tokenValidationParameters;
   private readonly UserManager<IdentityUser> _userManager;
-  private readonly JwkManager _jwkManager;
 
   public IdTokenFactory(
     IdentityConfiguration identityConfiguration,
     TokenValidationParameters tokenValidationParameters,
     UserManager<IdentityUser> userManager,
-    JwkManager jwkManager)
+    JwkManager jwkManager,
+    ILogger<IdTokenFactory> logger)
+    : base(logger, identityConfiguration, tokenValidationParameters, jwkManager)
   {
-    _identityConfiguration = identityConfiguration;
-    _tokenValidationParameters = tokenValidationParameters;
     _userManager = userManager;
-    _jwkManager = jwkManager;
   }
 
   public async Task<string> GenerateTokenAsync(string clientId, IEnumerable<string> scopes,
       string nonce, string userId)
   {
-    var iat = DateTimeOffset.UtcNow;
-    var exp = iat + TimeSpan.FromSeconds(_identityConfiguration.IdTokenExpiration);
+    var expires = DateTime.Now + TimeSpan.FromSeconds(_identityConfiguration.IdTokenExpiration);
+    var audience = clientId;
     var claims = new List<Claim>
     {
       new(JwtRegisteredClaimNames.Sub, userId),
-      new(JwtRegisteredClaimNames.Aud, clientId),
-      new(JwtRegisteredClaimNames.Iss, _identityConfiguration.InternalIssuer),
-      new(JwtRegisteredClaimNames.Iat, iat.ToString()),
-      new(JwtRegisteredClaimNames.Exp, exp.ToString()),
-      new(JwtRegisteredClaimNames.Nbf, iat.ToString()),
-      new(JwtRegisteredClaimNames.AuthTime, DateTimeOffset.Now.ToUnixTimeSeconds().ToString()),
-      new(JwtRegisteredClaimNames.Nonce, nonce),
-      new("scope", scopes.Aggregate((elem, acc) => $"{acc} {elem}"))
+      new("scope", string.Join(' ', scopes)),
+      new(JwtRegisteredClaimNames.Iat, DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString())
     };
+
+    if (!string.IsNullOrWhiteSpace(nonce))
+      claims.Add(new(JwtRegisteredClaimNames.Nonce, nonce));
+
     var user = await _userManager.FindByIdAsync(userId);
     var userClaims = await _userManager.GetClaimsAsync(user);
     var userRoles = await _userManager.GetRolesAsync(user);
     claims.AddRange(userClaims.Select(claim => new Claim(claim.Type, claim.Value)));
     claims.Add(new Claim("roles", JsonSerializer.Serialize(userRoles)));
 
-    var key = new RsaSecurityKey(_jwkManager.RsaCryptoServiceProvider)
-    {
-      KeyId = _jwkManager.KeyId
-    };
-    var signingCredentials = new SigningCredentials(key, SecurityAlgorithms.RsaSha256);
-    var securityToken = new JwtSecurityToken(
-        claims: claims,
-        signingCredentials: signingCredentials);
-
-    var token = new JwtSecurityTokenHandler().WriteToken(securityToken);
-    return await Task.FromResult(token);
-  }
-
-  public Task<JwtSecurityToken> DecodeTokenAsync(string token)
-  {
-    new JwtSecurityTokenHandler()
-        .ValidateToken(token, _tokenValidationParameters, out var validatedToken);
-    return Task.FromResult((JwtSecurityToken)validatedToken);
-  }
-
-  public async Task<bool> ValidateTokenAsync(string token)
-  {
-    new JwtSecurityTokenHandler()
-        .ValidateToken(token, _tokenValidationParameters, out _);
-    return await Task.FromResult(true);
+    return GetSignedToken(claims, audience, expires);
   }
 }
