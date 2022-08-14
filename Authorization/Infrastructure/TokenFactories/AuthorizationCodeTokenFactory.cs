a8bc66893372c0b8536eb7d97f3b72285f232d90
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2022-08-14 14:53:43 +0200

    update: code factory to use json serialization and cleanup obsolete properties

--- Authorization/Infrastructure/TokenFactories/AuthorizationCodeTokenFactory.cs
+++ Authorization/Infrastructure/TokenFactories/AuthorizationCodeTokenFactory.cs
@@ -1,110 +1,92 @@
 using AuthorizationServer.Tokens;
 using Microsoft.AspNetCore.DataProtection;
-using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 using System.Security.Cryptography;
 using System.Text;
 using System.Text.Json;
 
 namespace AuthorizationServer.TokenFactories;
-
 public class AuthorizationCodeTokenFactory
 {
   private readonly IdentityConfiguration _identityConfiguration;
+  private readonly UserManager<IdentityUser> _userManager;
   private readonly IDataProtector _protector;
 
   public AuthorizationCodeTokenFactory(
     IdentityConfiguration identityConfiguration,
-    IDataProtectionProvider protectorProvider)
+    IDataProtectionProvider protectorProvider,
+    UserManager<IdentityUser> userManager)
   {
     _identityConfiguration = identityConfiguration;
+    _userManager = userManager;
     _protector = protectorProvider.CreateProtector(identityConfiguration.AuthorizationCodeSecret);
   }
 
   public async Task<string> GenerateTokenAsync(
-      [Required] string redirectUri,
-      [Required] ICollection<string> scopes,
-      [Required] string clientId,
-      [Required] string codeChallenge,
-      [Required] string codeChallengeMethod,
-      [Required] string userId)
+    string redirectUri,
+    ICollection<string> scopes,
+    string clientId,
+    string codeChallenge,
+    string userId,
+    string nonce)
   {
     var ms = new MemoryStream();
     await using var writer = new BinaryWriter(ms, Encoding.UTF8, false);
-    writer.Write(DateTimeOffset.UtcNow.UtcTicks);
-    writer.Write(redirectUri);
-    writer.Write(JsonSerializer.Serialize(scopes));
-    writer.Write(clientId);
-    writer.Write(codeChallenge);
-    writer.Write(codeChallengeMethod);
-    writer.Write(userId);
-    writer.Write("authorization_code");
+    var code = new AuthorizationCode 
+    {
+      CreatedAt = DateTimeOffset.UtcNow.Ticks,
+      ClientId = clientId,
+      RedirectUri = redirectUri,
+      Scopes = scopes,
+      CodeChallenge = codeChallenge,
+      UserId = userId,
+      Nonce = nonce
+    };
+    writer.Write(JsonSerializer.Serialize(code));
     var protectedBytes = _protector.Protect(ms.ToArray());
-    return Convert.ToBase64String(protectedBytes);
+    return Base64UrlEncoder.Encode(protectedBytes);
   }
 
-  public Task<AuthorizationCode> DecodeTokenAsync(string token)
+  public AuthorizationCode DecodeToken(string token)
   {
-    var decoded = Convert.FromBase64String(token);
+    var decoded = Base64UrlEncoder.DecodeBytes(token);
     var unProtectedBytes = _protector.Unprotect(decoded);
     var ms = new MemoryStream(unProtectedBytes);
     using var reader = new BinaryReader(ms, Encoding.UTF8, false);
-    var time = new DateTimeOffset(reader.ReadInt64(), TimeSpan.Zero);
-    var redirectUri = reader.ReadString();
-    var scopes = JsonSerializer.Deserialize<ICollection<string>>(reader.ReadString());
-    var clientId = reader.ReadString();
-    var codeChallenge = reader.ReadString();
-    var codeChallengeMethod = reader.ReadString();
-    var userId = reader.ReadString();
-    var purpose = reader.ReadString();
-    return Task.FromResult(new AuthorizationCode
-    {
-      RedirectUri = redirectUri,
-      Scopes = scopes,
-      ClientId = clientId,
-      CodeChallenge = codeChallenge,
-      UserId = userId
-    });
+    var code = reader.ReadString();
+    return JsonSerializer.Deserialize<AuthorizationCode>(code) 
+      ?? throw new Exception("AuthorizationCode is not valid");
   }
 
-  public Task<bool> ValidateAsync(
-      [Required] string purpose,
-      [Required] string token,
-      [Required] string redirectUri,
-      [Required] string clientId,
-      [Required] string codeVerifier)
+  public async Task<bool> ValidateAsync(
+   string token,
+   string redirectUri,
+   string clientId,
+   string codeVerifier)
   {
-    var decoded = Convert.FromBase64String(token);
-    var unProtectedBytes = _protector.Unprotect(decoded);
-    var ms = new MemoryStream(unProtectedBytes);
-    using var reader = new BinaryReader(ms, Encoding.Default, false);
-    var creationTime = new DateTimeOffset(reader.ReadInt64(), TimeSpan.Zero);
+    var code = DecodeToken(token);
+    var creationTime = new DateTimeOffset(code.CreatedAt, TimeSpan.Zero);
     if (creationTime + TimeSpan.FromSeconds(_identityConfiguration.AuthorizationCodeExpiration) <
         DateTimeOffset.UtcNow)
-      return Task.FromResult(false);
+      return false;
 
-    var creationRedirectUri = reader.ReadString();
-    if (!creationRedirectUri.Equals(redirectUri))
-      return Task.FromResult(false);
+    if (!code.RedirectUri.Equals(redirectUri))
+      return false;
 
-    var creationScopes = reader.ReadString();
+    if (!code.ClientId.Equals(clientId))
+      return false;
 
-    var creationClientId = reader.ReadString();
-    if (!creationClientId.Equals(clientId))
-      return Task.FromResult(false);
-
-    var codeChallenge = reader.ReadString();
-    var codeChallengeMethod = reader.ReadString();
     using var sha256 = SHA256.Create();
     var hashed = sha256.ComputeHash(Encoding.Default.GetBytes(codeVerifier));
-    var encoded = Microsoft.IdentityModel.Tokens.Base64UrlEncoder.Encode(hashed);
-    if (!codeChallenge.Equals(encoded))
-      return Task.FromResult(false);
+    var encoded = Base64UrlEncoder.Encode(hashed);
+    if (!code.CodeChallenge.Equals(encoded))
+      return false;
 
-    var userId = reader.ReadString();
-    var creationPurpose = reader.ReadString();
-    if (!creationPurpose.Equals(purpose))
-      return Task.FromResult(false);
+    var user = await _userManager.FindByIdAsync(code.UserId);
+    if (user is null)
+      return false;
 
-    return Task.FromResult(true);
+    return true;
   }
 }
--- Authorization/Infrastructure/Tokens/AuthorizationCode.cs
+++ Authorization/Infrastructure/Tokens/AuthorizationCode.cs
@@ -2,6 +2,7 @@ namespace AuthorizationServer.Tokens;
 
 public record AuthorizationCode
 {
+  public long CreatedAt { get; init; }
   public string ClientId { get; init; }
 
   public string RedirectUri { get; init; }
@@ -11,4 +12,6 @@ public record AuthorizationCode
   public string CodeChallenge { get; init; }
 
   public string UserId { get; init; }
+
+  public string Nonce { get; init; }
 }
