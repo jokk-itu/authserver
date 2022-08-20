cfaede951c38151617dea3571a08bd82c68556a9
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2022-08-20 18:40:35 +0200

    update: posttoken to explicitly use form as content type

--- Authorization/Contracts/PostToken/PostTokenRequest.cs
+++ Authorization/Contracts/PostToken/PostTokenRequest.cs
@@ -1,19 +1,29 @@
-﻿namespace Contracts.PostToken;
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Contracts.PostToken;
 public class PostTokenRequest
 {
-  public string GrantType { get; set; }
+  [FromForm(Name = "grant_type")]
+  public string GrantType { get; set; } = null!;
 
+  [FromForm(Name = "code")]
   public string? Code { get; set; }
 
+  [FromForm(Name = "client_id")]
   public string? ClientId { get; set; }
 
+  [FromForm(Name = "client_secret")]
   public string? ClientSecret { get; set; }
 
+  [FromForm(Name = "redirect_uri")]
   public string? RedirectUri { get; set; }
 
+  [FromForm(Name = "scope")]
   public string? Scope { get; set; }
 
+  [FromForm(Name = "code_verifier")]
   public string? CodeVerifier { get; set; }
 
+  [FromForm(Name = "refresh_token")]
   public string? RefreshToken { get; set; }
 }
