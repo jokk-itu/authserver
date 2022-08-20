f66a647d643594685393d465a359f4bcc605321b
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2022-08-20 23:21:55 +0200

    add: tokentypesconstants with access token and refresh token

---
+++ Authorization/Domain/Constants/TokenTypeConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Constants;
+public static class TokenTypeConstants
+{
+  public const string RefreshToken = "refresh_token";
+  public const string AccessToken = "access_token";
+}
