b8537cc8ee55d297adb627c2e4f74efed1a3935b
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2022-09-03 16:15:22 +0200

    add: accesstokenfactory tests

--- Authorization/Infrastructure/Factories/TokenFactories/AccessTokenFactory.cs
+++ Authorization/Infrastructure/Factories/TokenFactories/AccessTokenFactory.cs
@@ -24,7 +24,11 @@ public class AccessTokenFactory : TokenFactory
     _resourceManager = resourceManager;
   }
 
-  public async Task<string> GenerateTokenAsync(string clientId, ICollection<string> scopes, string userId, CancellationToken cancellationToken = default)
+  public async Task<string> GenerateTokenAsync(
+    string clientId, 
+    ICollection<string> scopes, 
+    string userId, 
+    CancellationToken cancellationToken = default)
   {
     var expires = DateTime.Now + TimeSpan.FromSeconds(_identityConfiguration.AccessTokenExpiration);
     var resources = await _resourceManager.ReadResourcesAsync(scopes, cancellationToken);
