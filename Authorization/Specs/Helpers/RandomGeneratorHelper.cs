89a55d7e12a5f44fdc22a23142ad45df99adc4e9
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2022-09-04 13:43:32 +0200

    add: random string generator

---
+++ Authorization/Specs/Helpers/RandomGeneratorHelper.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Security.Cryptography;
+
+namespace Specs.Helpers;
+public static class RandomGeneratorHelper
+{
+  public static string GeneratorRandomString(int length)
+  {
+    return Base64UrlEncoder.Encode(RandomNumberGenerator.GetBytes(length));
+  }
+}
