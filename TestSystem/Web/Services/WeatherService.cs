a8e7f46089f24137acb476f8c1c526fbe5bc1a9e
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2022-09-08 21:55:08 +0200

    update: weatherservice to return weather data

--- TestSystem/Web/Services/WeatherService.cs
+++ TestSystem/Web/Services/WeatherService.cs
@@ -11,11 +11,6 @@ public class WeatherService
 
   public async Task<string> GetSecretAsync()
   {
-    return await _httpClient.GetStringAsync("api/secret");
-  }
-
-  public async Task<string> GetAnonymousAsync()
-  {
-    return await _httpClient.GetStringAsync("api/anonymous");
+    return await _httpClient.GetStringAsync("api/weather");
   }
 }
