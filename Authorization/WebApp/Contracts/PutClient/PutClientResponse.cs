e0645f0004c87c58af86dc9769b54c27766ff1e4
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2022-09-17 16:27:08 +0200

    add: putclient

---
+++ Authorization/WebApp/Contracts/PutClient/PutClientResponse.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+using WebApp.Constants;
+
+namespace WebApp.Contracts.PutClient;
+
+#nullable disable
+public class PutClientResponse
+{
+  [JsonPropertyName(ParameterNames.ClientId)]
+  public string ClientId { get; init; }
+
+  [JsonPropertyName(ParameterNames.ClientSecret)]
+  public string ClientSecret { get; init; }
+
+  [JsonPropertyName(ParameterNames.ClientSecretExpiresAt)]
+  public int ClientSecretExpiresAt { get; init; }
+
+  [JsonPropertyName(ParameterNames.RegistrationAccessToken)]
+  public string RegistrationAccessToken { get; init; }
+
+  [JsonPropertyName(ParameterNames.RegistrationClientUri)]
+  public string RegistrationClientUri { get; init; }
+}
