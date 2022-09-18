bda6861e0218d71c716320617fb4e4f81a23a828
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2022-09-18 21:57:22 +0200

    add: get initial token contracts

---
+++ Authorization/WebApp/Contracts/GetResource/GetResourceResponse.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+using WebApp.Constants;
+
+namespace WebApp.Contracts.GetResource;
+
+#nullable disable
+public class GetResourceResponse
+{
+  [JsonPropertyName(ParameterNames.ResourceId)]
+  public string ResourceId { get; init; }
+
+  [JsonPropertyName(ParameterNames.ResourceName)]
+  public string ResourceName { get; init; }
+
+  [JsonPropertyName(ParameterNames.ResourceSecret)]
+  public string ResourceSecret { get; init; }
+
+  [JsonPropertyName(ParameterNames.Scope)]
+  public string Scope { get; init; }
+}
---
+++ Authorization/WebApp/Contracts/GetResourceInitialAccessToken/GetResourceInitialAccessToken.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+using WebApp.Constants;
+
+namespace WebApp.Contracts.GetResourceInitialAccessToken;
+
+#nullable disable
+public class GetResourceInitialAccessToken
+{
+  [JsonPropertyName(ParameterNames.AccessToken)]
+  public string AccessToken { get; init; }
+
+  [JsonPropertyName(ParameterNames.ExpiresIn)]
+  public int ExpiresIn { get; init; }
+}
