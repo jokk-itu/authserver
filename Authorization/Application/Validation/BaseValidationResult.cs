8f6cfa55a5d226904370c06b9d06fb99370aa48e
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2022-09-18 20:40:48 +0200

    add: base validators

---
+++ Authorization/Application/Validation/BaseValidationResult.cs
@@ -0,0 +1,23 @@
+﻿namespace Application.Validation;
+
+public class BaseValidationResult
+{
+  public BaseValidationResult()
+  {
+  }
+
+  public BaseValidationResult(string? errorCode, string? errorDescription)
+  {
+    ErrorCode = errorCode;
+    ErrorDescription = errorDescription;
+  }
+
+  public string? ErrorCode { get; init; }
+
+  public string? ErrorDescription { get; init; }
+
+  public bool IsError()
+  {
+    return !string.IsNullOrWhiteSpace(ErrorCode);
+  }
+}
---
+++ Authorization/Infrastructure/BaseValidators/ScopeValidator.cs
@@ -0,0 +1,19 @@
+﻿using Application;
+using Application.Validation;
+using Domain.Constants;
+
+namespace Infrastructure.BaseValidators;
+public class ScopeValidator : BaseValidator<ICollection<string>>
+{
+  public ScopeValidator(bool isRequired) : base(isRequired)
+  {
+  }
+
+  public override Task<BaseValidationResult> IsValidAsync(ICollection<string> value)
+  {
+    if (IsRequired && value.All(x => x != ScopeConstants.OpenId))
+      return GetInvalidResult(ErrorCode.InvalidScope, $"scope must at least contain {ScopeConstants.OpenId}");
+
+    return GetValidResult();
+  }
+}
