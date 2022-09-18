5503832c3ac336c55a63649b1bdf77ce5b589774
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2022-09-18 20:06:40 +0200

    add: authorizationconstants for authorization policies

---
+++ Authorization/WebApp/Constants/AuthorizationConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApp.Constants;
+
+public static class AuthorizationConstants
+{
+  public const string ClientRegistration = "ClientRegistration";
+  public const string ClientConfiguration = "ClientConfiguration";
+  public const string ResourceRegistration = "ResourceRegistration";
+  public const string ResourceConfiguration = "ResourceConfiguration";
+  public const string ScopeRegistration = "ScopeRegistration";
+  public const string ScopeConfiguration = "ScopeConfiguration";
+}
