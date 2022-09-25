28e51a5f07ba69aa52a6a555825c9eff66aa9213
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2022-09-25 13:00:38 +0200

    add: delete client endpoint integration test

--- Authorization/Specs/Controllers/ClientControllerTests.cs
+++ Authorization/Specs/Controllers/ClientControllerTests.cs
@@ -2,6 +2,7 @@
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using Domain.Constants;
+using Infrastructure.Requests.DeleteClient;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using WebApp.Contracts.GetClientInitialAccessToken;
@@ -43,5 +44,34 @@ public class ClientControllerTests : IClassFixture<WebApplicationFactory<Program
     // Assert
     Assert.Equal(HttpStatusCode.Created, response.StatusCode);
     Assert.NotNull(postClientResponse);
+  }
+
+  [Fact]
+  [Trait("Category", "Integration")]
+  public async Task DeleteClientAsync_ExpectOk()
+  {
+    // Arrange
+    var httpClient = _applicationFactory.CreateClient();
+    var initialTokenResponse = await httpClient.GetFromJsonAsync<GetClientInitialAccessTokenResponse>("connect/client/initial-token");
+    var postClientRequest = new HttpRequestMessage(HttpMethod.Post, "connect/client/register");
+    postClientRequest.Content = JsonContent.Create(new PostClientRequest
+    {
+      RedirectUris = new[] { "http://localhost:5002/callback" },
+      Scope = $"{ScopeConstants.OpenId}",
+      GrantTypes = new[] { OpenIdConnectGrantTypes.AuthorizationCode, OpenIdConnectGrantTypes.RefreshToken },
+      ClientName = "test"
+    });
+    postClientRequest.Headers.Authorization = new AuthenticationHeaderValue("Bearer", initialTokenResponse!.AccessToken);
+    var postClientResponse = await httpClient.PostAsJsonAsync("connect/client/register", postClientRequest);
+    var client = await postClientResponse.Content.ReadFromJsonAsync<PostClientResponse>();
+
+    var deleteClientRequest = new HttpRequestMessage(HttpMethod.Delete, "connect/client/configuration");
+    deleteClientRequest.Headers.Authorization = new AuthenticationHeaderValue("Bearer", client!.RegistrationAccessToken);
+
+    // Act
+    var deleteClientResponse = await httpClient.SendAsync(deleteClientRequest);
+
+    // Assert
+    Assert.Equal(HttpStatusCode.NoContent, deleteClientResponse.StatusCode);
   }
 }
