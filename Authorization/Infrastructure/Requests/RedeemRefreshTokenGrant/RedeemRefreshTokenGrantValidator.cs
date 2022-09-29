2175aaf49d04138a4b87a192a69548ce5b4a5ad3
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2022-09-29 22:03:30 +0200

    add: redeem refreshtoken to renew tokens

---
+++ Authorization/Infrastructure/Requests/RedeemRefreshTokenGrant/RedeemRefreshTokenGrantValidator.cs
@@ -0,0 +1,49 @@
+﻿using System.Net;
+using Application;
+using Application.Validation;
+using Domain;
+using Infrastructure.Decoders.Abstractions;
+using Infrastructure.Requests.CreateRefreshTokenGrant;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Requests.RedeemRefreshTokenGrant;
+public class RedeemRefreshTokenGrantValidator : IValidator<RedeemRefreshTokenGrantCommand>
+{
+  private readonly IdentityContext _identityContext;
+  private readonly ITokenDecoder _tokenDecoder;
+
+  public RedeemRefreshTokenGrantValidator(IdentityContext identityContext, ITokenDecoder tokenDecoder)
+  {
+    _identityContext = identityContext;
+    _tokenDecoder = tokenDecoder;
+  }
+
+  public async Task<ValidationResult> ValidateAsync(RedeemRefreshTokenGrantCommand value, CancellationToken cancellationToken = default)
+  {
+    if (await IsClientInvalidAsync(value))
+      return new ValidationResult(ErrorCode.InvalidClient, "client is invalid", HttpStatusCode.BadRequest);
+
+    if (IsInvalidRefreshToken(value))
+      return new ValidationResult(ErrorCode.InvalidRequest, "refresh_token is invalid", HttpStatusCode.BadRequest);
+
+    return new ValidationResult(HttpStatusCode.OK);
+  }
+
+  private async Task<bool> IsClientInvalidAsync(RedeemRefreshTokenGrantCommand command)
+  {
+    var client = await _identityContext
+      .Set<Client>()
+      .SingleOrDefaultAsync(x => x.Id == command.ClientId 
+                                 && x.Secret == command.ClientSecret);
+    return client is null;
+  }
+
+  private bool IsInvalidRefreshToken(RedeemRefreshTokenGrantCommand command)
+  {
+    if (string.IsNullOrWhiteSpace(command.RefreshToken))
+      return true;
+
+    var refreshToken = _tokenDecoder.DecodeToken(command.RefreshToken);
+    return refreshToken is null;
+  }
+}
