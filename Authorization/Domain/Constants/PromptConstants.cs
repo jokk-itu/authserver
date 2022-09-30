482d4f867bd4ef7648333f36266a3c4c13e873d9
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2022-09-30 12:38:55 +0200

    add: prompconstants

---
+++ Authorization/Domain/Constants/PromptConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Constants;
+public static class PromptConstants
+{
+  public const string None = "none";
+  public const string Login = "login";
+  public const string Consent = "consent";
+  public const string SelectAccount = "select_account";
+}
