68cb0f8adfcba33a6df9b053055efe27c7fe461b
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2022-11-20 21:18:32 +0100

    add: baseunit test abstract class with in-memory identitycontext

---
+++ Authorization/Specs/BaseUnitTest.cs
@@ -0,0 +1,21 @@
+﻿using Infrastructure;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace Specs;
+public abstract class BaseUnitTest
+{
+
+  protected readonly IdentityContext _identityContext;
+
+  protected BaseUnitTest()
+  {
+    var connection = new SqliteConnection("DataSource=:memory:");
+    connection.Open();
+    var options = new DbContextOptionsBuilder<IdentityContext>()
+      .UseSqlite(connection)
+      .Options;
+    _identityContext = new IdentityContext(options);
+    _identityContext.Database.EnsureCreated();
+  }
+}
