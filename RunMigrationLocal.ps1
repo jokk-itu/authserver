89342c18a38cf6821a4d415dfd695073765348a4
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2022-11-20 21:20:00 +0100

    remove: old migration scripts

--- RunMigrationLocal.ps1
+++
@@ -1,3 +0,0 @@
-Push-Location Authorization/Infrastructure
-dotnet ef database update -- "SqlServer" "Server=localhost,1433;Initial Catalog=Identity;Trusted_Connection=False;User ID=sa;Password=Password12!"
-Pop-Location
