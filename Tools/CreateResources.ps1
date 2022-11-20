7d494a501929eed56028e7196c309a9dfa6792fe
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2022-11-20 21:14:44 +0100

    add: create client, scope and resource scripts

---
+++ Tools/CreateResources.ps1
@@ -0,0 +1,22 @@
+function GetInitialToken([Parameter(Mandatory)][string]$Uri) {
+    $response = Invoke-WebRequest -Uri $Uri -Method 'GET'
+    $token = ConvertFrom-Json $response.Content | Select -Expand 'access_token'
+    return $token
+}
+
+function PostResource([Parameter()][string]$Uri, [Parameter(Mandatory)][string]$Token, [Parameter(Mandatory)][string]$Name, [Parameter(Mandatory)][string]$Scope) {
+    $body = @{
+        'resource_name' = $Name
+        'scope' = $Scope
+    }
+    $headers = @{
+        'Authorization' = "Bearer $($Token)"
+    }
+    $response = Invoke-WebRequest -Uri $Uri -Method 'POST' -Headers $headers -Body ($body | ConvertTo-Json) -ContentType 'application/json'
+    return $response.Content
+}
+$token = GetInitialToken 'http://localhost:5000/connect/resource/initial-token'
+
+PostResource 'http://localhost:5000/connect/resource/register' $token 'weather' 'weather:read'
+
+PostResource 'http://localhost:5000/connect/resource/register' $token 'identityprovider' 'identityprovider:read'
---
+++ Tools/CreateScopes.ps1
@@ -0,0 +1,20 @@
+function GetInitialToken([Parameter(Mandatory)][string]$Uri) {
+    $response = Invoke-WebRequest -Uri $Uri -Method 'GET'
+    $token = ConvertFrom-Json $response.Content | Select -Expand 'access_token'
+    return $token
+}
+
+function PostScope([Parameter(Mandatory)][string]$Uri, [Parameter(Mandatory)][string]$Token,[Parameter(Mandatory)][string]$ScopeName) {
+    $body = @{
+        'scope_name' = $ScopeName
+    }
+    $headers = @{
+        'Authorization' = "Bearer $($Token)"
+    }
+    $response = Invoke-WebRequest -Uri $Uri -Method 'POST' -Headers $headers -Body ($body | ConvertTo-Json) -ContentType 'application/json'
+    return $response.Content
+}
+
+$token = GetInitialToken 'http://localhost:5000/connect/scope/initial-token'
+PostScope 'http://localhost:5000/connect/scope/register' $token 'weather:read'
+PostScope 'http://localhost:5000/connect/scope/register' $token 'identityprovider:read'
