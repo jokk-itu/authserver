fda1ab9af0c14dde7ed231c91bba9af8373c393e
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2022-12-12 21:43:48 +0100

    update: form builder with error form

--- Authorization/WebApp/FormPostBuilder.cs
+++ Authorization/WebApp/FormPostBuilder.cs
@@ -4,6 +4,11 @@ using WebApp.Constants;
 namespace WebApp;
 public static class FormPostBuilder
 {
+  private const string _end = @"
+</form>
+</body>
+</html>";
+
   public static string BuildAuthorizationCodeResponse(string redirectUri, string state, string code, string iss)
   {
     var formBuilder = new StringBuilder();
@@ -11,7 +16,26 @@ public static class FormPostBuilder
     formBuilder.Append(AddInput(ParameterNames.State, state));
     formBuilder.Append(AddInput(ParameterNames.Code, code));
     formBuilder.Append(AddInput(ParameterNames.Issuer, iss));
-    formBuilder.Append(AddEnd());
+    formBuilder.Append(_end);
+    return formBuilder.ToString();
+  }
+
+  public static string BuildErrorResponse(string redirectUri, string state, string? error, string? errorDescription, string iss)
+  {
+    var formBuilder = new StringBuilder();
+    formBuilder.Append(AddStart(redirectUri));
+    formBuilder.Append(AddInput(ParameterNames.State, state));
+    if (!string.IsNullOrWhiteSpace(error))
+    {
+      formBuilder.Append(AddInput(ParameterNames.Error, error));
+    }
+
+    if (!string.IsNullOrWhiteSpace(errorDescription))
+    {
+      formBuilder.Append(AddInput(ParameterNames.ErrorDescription, errorDescription));
+    }
+    formBuilder.Append(AddInput(ParameterNames.Issuer, iss));
+    formBuilder.Append(_end);
     return formBuilder.ToString();
   }
 
@@ -27,14 +51,6 @@ public static class FormPostBuilder
 <p>Redirecting to client</p>
 <form method=""post"" action=""{redirectUri}"">
 ";
-  }
-
-  private static string AddEnd()
-  {
-    return @"
-</form>
-</body>
-</html>";
   }
 
   private static string AddInput(string name, string value)
