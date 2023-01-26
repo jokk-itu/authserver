345bee22c733590aa7ccaf7a3ac39f971e7a7f6f
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2023-01-26 15:35:44 +0100

    update: format code

--- Authorization/WebApp/Controllers/OAuthControllerBase.cs
+++ Authorization/WebApp/Controllers/OAuthControllerBase.cs
@@ -15,24 +15,27 @@ public abstract class OAuthControllerBase : Controller
 
     protected IActionResult BadOAuthResult(string? error, string? errorDescription)
     {
-        var response = new ErrorResponse();
+      var response = new ErrorResponse();
+      if (!string.IsNullOrWhiteSpace(error))
+      {
+        response.Error = error;
+      }
 
-        if (!string.IsNullOrWhiteSpace(error))
-            response.Error = error;
+      if (!string.IsNullOrWhiteSpace(errorDescription))
+      {
+        response.ErrorDescription = errorDescription;
+      }
 
-        if (!string.IsNullOrWhiteSpace(errorDescription))
-            response.ErrorDescription = errorDescription;
-
-        return BadRequest(response);
+      return BadRequest(response);
     }
 
     protected IActionResult AuthorizationCodeFormPostResult(string redirectUri, string state, string code)
     {
-        return new ContentResult
-        {
-            ContentType = "text/html",
-            Content = FormPostBuilder.BuildAuthorizationCodeResponse(redirectUri, state, code, _identityConfiguration.Issuer)
-        };
+      return new ContentResult
+      {
+        ContentType = "text/html",
+        Content = FormPostBuilder.BuildAuthorizationCodeResponse(redirectUri, state, code, _identityConfiguration.Issuer)
+      };
     }
 
     protected IActionResult ErrorFormPostResult(string redirectUri, string state, string? error, string? errorDescription)
@@ -46,10 +49,10 @@ public abstract class OAuthControllerBase : Controller
 
     protected IActionResult LoginFormPostResult()
     {
-        return new ContentResult
-        {
-            ContentType = "text/html",
-            Content = FormPostBuilder.BuildLoginCodeResponse()
-        };
+      return new ContentResult
+      {
+        ContentType = "text/html",
+        Content = FormPostBuilder.BuildLoginCodeResponse()
+      };
     }
 }
