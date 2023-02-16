3bb56913b5d9c848eaaa8cf006ae44fb6503334c
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2023-02-16 18:44:03 +0100

    update: setup cdn for css and add oidc client

---
+++ Clients/Blazor.Hybrid/HybridApp/AuthenticationBrowser.cs
@@ -0,0 +1,39 @@
+﻿using IdentityModel.Client;
+using IdentityModel.OidcClient.Browser;
+
+namespace HybridApp;
+public class AuthenticationBrowser : IdentityModel.OidcClient.Browser.IBrowser
+{
+  public async Task<BrowserResult> InvokeAsync(BrowserOptions options, CancellationToken cancellationToken = new CancellationToken())
+  {
+    try
+    {
+      var result = await WebAuthenticator.Default.AuthenticateAsync(
+        new Uri(options.StartUrl),
+        new Uri(options.EndUrl));
+
+      var url = new RequestUrl("hybridapp://callback")
+        .Create(new Parameters(result.Properties));
+
+      return new BrowserResult
+      {
+        Response = url,
+        ResultType = BrowserResultType.Success
+      };
+    }
+    catch (TaskCanceledException)
+    {
+      return new BrowserResult
+      {
+        ResultType = BrowserResultType.UserCancel
+      };
+    }
+    catch (Exception)
+    {
+      return new BrowserResult
+      {
+        ResultType = BrowserResultType.UnknownError
+      };
+    }
+  }
+}
--- Clients/Blazor.Hybrid/HybridApp/MauiProgram.cs
+++ Clients/Blazor.Hybrid/HybridApp/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using HybridApp.Data;
+using IdentityModel.OidcClient;
 
 namespace HybridApp;
 public static class MauiProgram
@@ -19,6 +20,15 @@ public static class MauiProgram
 #endif
 
     builder.Services.AddSingleton<WeatherForecastService>();
+
+    builder.Services.AddSingleton(new OidcClient(new OidcClientOptions
+    {
+      Authority = "https://localhost:5000",
+      ClientId = "",
+      Scope = "openid profile email phone identityprovider:read weather:read",
+      RedirectUri = "hybridapp://callback",
+      Browser = new AuthenticationBrowser()
+    }));
 
     return builder.Build();
   }
