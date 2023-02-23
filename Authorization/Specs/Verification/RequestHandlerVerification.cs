d431c7cacae619d2396af9c61057cce3e10648bb
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2023-02-23 20:16:20 +0100

    add: verification testing of requests

---
+++ Authorization/Specs/Verification/RequestHandlerVerification.cs
@@ -0,0 +1,27 @@
+﻿using Infrastructure.Requests;
+using MediatR;
+using Xunit;
+
+namespace Specs.Verification;
+public class RequestHandlerVerification
+{
+  [Fact]
+  public void RequireAllRequestsToHaveARequestsHandler()
+  {
+    var requests = typeof(Response).Assembly
+      .GetTypes()
+      .Where(x => x.IsClass)
+      .Where(x => x.GetInterface(typeof(IRequest<>).Name) is not null)
+      .ToList();
+
+    var requestHandlers = typeof(Response).Assembly
+      .GetTypes()
+      .Where(x => x.IsClass)
+      .Where(x => x.GetInterface(typeof(IRequestHandler<,>).Name) is not null)
+      .Select(x => x.GetInterface(typeof(IRequestHandler<,>).Name))
+      .OfType<Type>()
+      .ToList();
+
+    Assert.True(requests.All(x => requestHandlers.Any(y => y.GenericTypeArguments[0] == x)));
+  }
+}
