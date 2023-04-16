c47c732d4b3e6d2a8e1e2ad1a4f3392969ed6853
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2023-04-16 21:15:42 +0200

    add: token introspection handler and validator

---
+++ Authorization/Infrastructure/Requests/TokenIntrospection/TokenIntrospectionHandler.cs
@@ -0,0 +1,55 @@
+﻿using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Requests.TokenIntrospection;
+public class TokenIntrospectionHandler : IRequestHandler<TokenIntrospectionQuery, TokenIntrospectionResponse>
+{
+  private readonly IdentityContext _identityContext;
+
+  public TokenIntrospectionHandler(IdentityContext identityContext)
+  {
+    _identityContext = identityContext;
+  }
+
+  public async Task<TokenIntrospectionResponse> Handle(TokenIntrospectionQuery request, CancellationToken cancellationToken)
+  {
+    var query = await _identityContext
+      .Set<Token>()
+      .Where(x => x.Reference == request.Token)
+      .OfType<GrantToken>()
+      .Select(x => new
+      {
+        Token = x,
+        x.AuthorizationGrant.Session.User.UserName,
+        Subject = x.AuthorizationGrant.Session.User.Id
+      })
+      .SingleOrDefaultAsync(cancellationToken: cancellationToken);
+
+    if (query is null)
+    {
+      return new TokenIntrospectionResponse(HttpStatusCode.OK)
+      {
+        Active = false
+      };
+    }
+
+    return new TokenIntrospectionResponse(HttpStatusCode.OK)
+    {
+      Active = query.Token.RevokedAt is null,
+      JwtId = query.Token.Id.ToString(),
+      ClientId = request.ClientId,
+      ExpiresAt = new DateTimeOffset(query.Token.ExpiresAt).ToUnixTimeSeconds(),
+      Issuer = query.Token.Issuer,
+      Audience = query.Token.Audience.Split(' '),
+      IssuedAt = new DateTimeOffset(query.Token.IssuedAt).ToUnixTimeSeconds(),
+      NotBefore = new DateTimeOffset(query.Token.NotBefore).ToUnixTimeSeconds(),
+      Scope = query.Token.Scope,
+      Subject = query.Subject,
+      TokenType = query.Token.TokenType.ToString(),
+      UserName = query.UserName
+    };
+  }
+}
---
+++ Authorization/Infrastructure/Requests/TokenIntrospection/TokenIntrospectionResponse.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+
+namespace Infrastructure.Requests.TokenIntrospection;
+public class TokenIntrospectionResponse : Response
+{
+  public TokenIntrospectionResponse(HttpStatusCode statusCode)
+    : base(statusCode)
+  {
+  }
+
+  public TokenIntrospectionResponse(string? errorCode, string? errorDescription, HttpStatusCode statusCode)
+    : base(errorCode, errorDescription, statusCode)
+  {
+  }
+
+  public bool Active { get; init; }
+  public string? Scope { get; init; }
+  public string? ClientId { get; init; }
+  public string? UserName { get; init; }
+  public string? TokenType { get; init; }
+  public long? ExpiresAt { get; init; }
+  public long? IssuedAt { get; init; }
+  public long? NotBefore { get; init; }
+  public string? Subject { get; init; }
+  public IEnumerable<string> Audience { get; init; } = new List<string>();
+  public string? Issuer { get; init; }
+  public string? JwtId { get; init; }
+}
