ef659cbcdb09a6d3fc28eaf1d6fc512e8dd18152
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2023-04-16 21:16:08 +0200

    add: token revocation handler and validator

---
+++ Authorization/Infrastructure/Requests/TokenRevocation/TokenRevocationHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Requests.TokenRevocation;
+public class TokenRevocationHandler : IRequestHandler<TokenRevocationCommand, TokenRevocationResponse>
+{
+  private readonly IdentityContext _identityContext;
+
+  public TokenRevocationHandler(IdentityContext identityContext)
+  {
+    _identityContext = identityContext;
+  }
+
+  public async Task<TokenRevocationResponse> Handle(TokenRevocationCommand request, CancellationToken cancellationToken)
+  {
+    var token = await _identityContext
+      .Set<Token>()
+      .Where(x => x.Reference == request.Token)
+      .Where(x => x.RevokedAt == null)
+      .SingleOrDefaultAsync(cancellationToken: cancellationToken);
+
+    if (token is null)
+    {
+      return new TokenRevocationResponse(HttpStatusCode.OK);
+    }
+
+    token.RevokedAt = DateTime.UtcNow;
+    await _identityContext.SaveChangesAsync(cancellationToken: cancellationToken);
+    return new TokenRevocationResponse(HttpStatusCode.OK);
+  }
+}
---
+++ Authorization/Infrastructure/Requests/TokenRevocation/TokenRevocationValidator.cs
@@ -0,0 +1,59 @@
+﻿using System.Net;
+using Application;
+using Application.Validation;
+using Domain;
+using Domain.Constants;
+using Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Requests.TokenRevocation;
+public class TokenRevocationValidator : IValidator<TokenRevocationCommand>
+{
+  private readonly IdentityContext _identityContext;
+
+  public TokenRevocationValidator(IdentityContext identityContext)
+  {
+    _identityContext = identityContext;
+  }
+
+  public async Task<ValidationResult> ValidateAsync(TokenRevocationCommand value, CancellationToken cancellationToken = default)
+  {
+    if (!TokenTypeConstants.TokenTypes.Contains(value.TokenTypeHint))
+    {
+      return new ValidationResult(ErrorCode.UnsupportedTokenType, "the given token_type_hint is not recognized", HttpStatusCode.BadRequest);
+    }
+
+    if (string.IsNullOrWhiteSpace(value.Token))
+    {
+      return new ValidationResult(ErrorCode.InvalidRequest, "token must not be null or empty",
+        HttpStatusCode.BadRequest);
+    }
+
+    var client = await _identityContext
+      .Set<Client>()
+      .Where(x => x.Id == value.ClientId)
+      .SingleOrDefaultAsync(cancellationToken: cancellationToken);
+
+    if (client is null
+        || client.TokenEndpointAuthMethod != TokenEndpointAuthMethod.None
+        && client.Secret != value.ClientSecret)
+    {
+      return new ValidationResult(ErrorCode.InvalidClient, "client could not be authenticated", HttpStatusCode.BadRequest);
+    }
+
+    var token = await _identityContext
+      .Set<Token>()
+      .OfType<GrantToken>()
+      .Where(x => x.Reference == value.Token)
+      .Include(x => x.AuthorizationGrant)
+      .ThenInclude(x => x.Client)
+      .SingleOrDefaultAsync(cancellationToken: cancellationToken);
+
+    if (token is not null && token.AuthorizationGrant.Client.Id != value.ClientId)
+    {
+      return new ValidationResult(ErrorCode.UnauthorizedClient, "client_id does not match token", HttpStatusCode.BadRequest);
+    }
+
+    return new ValidationResult(HttpStatusCode.OK);
+  }
+}
