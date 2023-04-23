9367ba0f0fcb20fb0e3e8308e213892f369a7ce8
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2023-04-23 12:00:24 +0200

    update: remove migration script to use correct path

--- Tools/RemoveMigration.ps1
+++ Tools/RemoveMigration.ps1
@@ -1,3 +1,3 @@
-Push-Location Authorization/Infrastructure
+Push-Location ../Authorization/Infrastructure
 dotnet ef migrations remove -- "SqlServer" "Server=localhost,1433;Initial Catalog=Identity;Trusted_Connection=False;User ID=sa;Password=Password12!"
 Pop-Location
