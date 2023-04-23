a464184cbcad59ffe360cd589412686f7ecc40a3
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2023-04-23 12:04:08 +0200

    remove: registration constants used for initial tokens

--- Authorization/Domain/Constants/ScopeConstants.cs
+++ Authorization/Domain/Constants/ScopeConstants.cs
@@ -6,13 +6,8 @@ public static class ScopeConstants
   public const string Email = "email";
   public const string Phone = "phone";
   public const string OpenId = "openid";
-  public const string ClientRegistration = "identityprovider:clientregistration";
   public const string ClientConfiguration = "identityprovider:clientconfiguration";
-  public const string ResourceRegistration = "identityprovider:resourceregistration";
   public const string ResourceConfiguration = "identityprovider:resourceconfiguration";
-  public const string ScopeRegistration = "identityprovider:scoperegistration";
   public const string ScopeConfiguration = "identityprovider:scopeconfiguration";
   public const string UserInfo = "identityprovider:userinfo";
-
-  public static readonly string[] PiiScopes = new[] { Profile, Email, Phone };
 }
