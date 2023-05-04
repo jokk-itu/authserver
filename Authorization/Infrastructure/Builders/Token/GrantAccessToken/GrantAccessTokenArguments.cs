0da418687f7275f4bc95d8dcabb183f04671d0ce
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2023-05-04 22:27:33 +0200

    add: grant accesstoken builder

---
+++ Authorization/Infrastructure/Builders/Token/GrantAccessToken/GrantAccessTokenArguments.cs
@@ -0,0 +1,8 @@
+﻿namespace Infrastructure.Builders.Token.GrantAccessToken;
+#nullable disable
+public class GrantAccessTokenArguments
+{
+  public IEnumerable<string> ResourceNames { get; init; } = new List<string>();
+  public string Scope { get; init; }
+  public string AuthorizationGrantId { get; init; }
+}
