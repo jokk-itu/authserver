c85430c534ea10bf63bb14b0ee928b0d466a0240
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2023-05-04 22:28:04 +0200

    add: logout token builder

---
+++ Authorization/Infrastructure/Builders/Token/LogoutToken/LogoutTokenArguments.cs
@@ -0,0 +1,8 @@
+﻿namespace Infrastructure.Builders.Token.LogoutToken;
+#nullable disable
+public class LogoutTokenArguments
+{
+  public string ClientId { get; init; }
+  public string UserId { get; init; }
+  public string SessionId { get; init; }
+}
