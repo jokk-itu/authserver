2c5e79ec60cf6b8066151c9a91719fdb9b412206
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2023-05-04 22:19:26 +0200

    add: token helper with isstructuredtoken function

---
+++ Authorization/Infrastructure/Helpers/TokenHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace Infrastructure.Helpers;
+public static class TokenHelper
+{
+  public static bool IsStructuredToken(string token)
+  {
+    return GetDotLength(token) is 3 or 5;
+  }
+
+  private static int GetDotLength(string token)
+  {
+    if (string.IsNullOrWhiteSpace(token))
+    {
+      throw new ArgumentException("must not be null or whitespace", token);
+    }
+
+    return token.Split('.').Length;
+  }
+}
