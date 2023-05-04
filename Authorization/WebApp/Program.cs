e528a70c466b7206a65999012ea4e51ab2461592
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2023-05-04 22:18:40 +0200

    update: do not redirect to https

--- Authorization/WebApp/Program.cs
+++ Authorization/WebApp/Program.cs
@@ -62,7 +62,6 @@ if (app.Environment.IsDevelopment())
   IdentityModelEventSource.ShowPII = true;
 }
 
-app.UseHttpsRedirection();
 app.UseHsts();
 app.UseSerilogRequestLogging();
 app.UseStaticFiles();
