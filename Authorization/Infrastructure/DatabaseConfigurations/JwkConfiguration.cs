1baae2bc83120dba3e48a5aa42fa55dd059d0602
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2023-05-05 22:18:22 +0200

    add: jwk configuration

---
+++ Authorization/Infrastructure/DatabaseConfigurations/JwkConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.DatabaseConfigurations;
+internal class JwkConfiguration : IEntityTypeConfiguration<Jwk>
+{
+  public void Configure(EntityTypeBuilder<Jwk> builder)
+  {
+    throw new NotImplementedException();
+  }
+}
