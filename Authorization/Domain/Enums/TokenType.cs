4413507b8be5c26aaf7f899313f5448ca5e9e421
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2023-05-14 22:06:15 +0200

    update: set description for token types

--- Authorization/Domain/Enums/TokenType.cs
+++ Authorization/Domain/Enums/TokenType.cs
@@ -1,8 +1,17 @@
-﻿namespace Domain.Enums;
+﻿using System.ComponentModel;
+using Domain.Constants;
+
+namespace Domain.Enums;
 public enum TokenType
 {
+  [Description(TokenTypeConstants.RefreshToken)]
   RefreshToken,
+
+  [Description(TokenTypeConstants.AccessToken)]
   ClientAccessToken,
+
+  [Description(TokenTypeConstants.AccessToken)]
   GrantAccessToken,
+
   RegistrationToken
 }
