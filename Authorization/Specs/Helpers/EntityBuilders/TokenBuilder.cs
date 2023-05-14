5d1abd5e24bc19b99da0721fc67d4059e0d2af7d
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2023-05-14 22:03:32 +0200

    add: token entity builder

---
+++ Authorization/Specs/Helpers/EntityBuilders/TokenBuilder.cs
@@ -0,0 +1,45 @@
+﻿using Domain;
+
+namespace Specs.Helpers.EntityBuilders;
+public abstract class TokenBuilder
+{
+  protected string _scope = string.Empty;
+  protected DateTime _expiresAt = DateTime.UtcNow.AddHours(1);
+  protected DateTime _issuedAt = DateTime.UtcNow;
+  protected DateTime _notBefore = DateTime.UtcNow;
+  protected string _audience = string.Empty;
+  protected string _issuer = string.Empty;
+  protected DateTime? _revokedAt;
+
+  public abstract Token Build();
+
+  public TokenBuilder AddScope(string scope)
+  {
+    _scope = scope;
+    return this;
+  }
+
+  public TokenBuilder AddExpiresAt(DateTime expiresAt)
+  {
+    _expiresAt = expiresAt;
+    return this;
+  }
+
+  public TokenBuilder AddAudience(string audience)
+  {
+    _audience = audience;
+    return this;
+  }
+
+  public TokenBuilder AddIssuer(string issuer)
+  {
+    _issuer = issuer;
+    return this;
+  }
+
+  public TokenBuilder AddRevokedAt(DateTime revokedAt)
+  {
+    _revokedAt = revokedAt;
+    return this;
+  }
+}
