274d8528ae668d6e63257d9146fd6a7e4230a57e
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2023-05-25 22:36:48 +0200

    update: logging for configuration and sqlite connectionstring

--- Authorization/ConfigurationApp/Program.cs
+++ Authorization/ConfigurationApp/Program.cs
@@ -4,14 +4,20 @@ using Domain;
 using Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Serilog;
 
 var builder = new ConfigurationBuilder()
   .SetBasePath(Directory.GetCurrentDirectory())
   .AddJsonFile("config.json", optional: false)
   .AddEnvironmentVariables();
 
+Log.Logger = new LoggerConfiguration()
+  .Enrich.FromLogContext()
+  .WriteTo.Console()
+  .CreateLogger();
+
 var config = builder.Build();
-var sqliteConnectionString = config.GetConnectionString("Sqlite");
+var sqliteConnectionString = config.GetConnectionString("SQLite");
 var dbContextOptionsBuilder = new DbContextOptionsBuilder<IdentityContext>();
 if (!string.IsNullOrWhiteSpace(sqliteConnectionString))
 {
@@ -25,23 +31,35 @@ if (args[0] == "resource")
   var resources = config.GetSection("Resources").Get<List<Resource>>();
   foreach (var resource in resources)
   {
+    Log.Information("Inserting resource {@resource}", resource);
+    foreach (var scopeName in resource.Scopes.Select(x => x.Name))
+    {
+      var scope = await identityContext
+        .Set<Scope>()
+        .SingleOrDefaultAsync(y => y.Name == scopeName);
+      resource.Scopes.Add(scope);
+    }
     await identityContext.AddAsync(resource);
   }
 
   await identityContext.SaveChangesAsync();
+  Log.Information("Resources inserted");
 }
 else if (args[0] == "scope")
 {
   var scopes = config.GetSection("Scopes").Get<List<Scope>>();
   foreach (var scope in scopes)
   {
+    Log.Information("Inserting scope {@scope}", scope);
     await identityContext.AddAsync(scope);
   }
 
   await identityContext.SaveChangesAsync();
+  Log.Information("Scopes inserted");
 }
 else if (args[0] == "rotate")
 {
+  Log.Information("Rotating JWK");
   using var rsa = new RSACryptoServiceProvider(4096);
   var privateKey = config.GetSection("IdentityConfiguration").GetValue<string>("PrivateKey");
   var password = Encoding.Default.GetBytes(privateKey);
@@ -57,12 +75,18 @@ else if (args[0] == "rotate")
   };
   await identityContext.Set<Jwk>().AddAsync(jwk);
   await identityContext.SaveChangesAsync();
+  Log.Information("Rotating JWK");
 }
 else if (args[0] == "migration")
 {
+  Log.Information("Migration starting");
   await identityContext.Database.MigrateAsync();
+  Log.Information("Migration finished");
 }
 else
 {
+  Log.CloseAndFlush();
   throw new NotSupportedException($"Argument is not supported: {args[0]}");
 }
+
+Log.CloseAndFlush();
