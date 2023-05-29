aebf31a3b4c60f8aeebeec199e1df72d2e9c1ba9
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2023-05-29 22:01:54 +0200

    add: registration service to client library

---
+++ OIDC/OIDC.Client/Registration/IRegistrationService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+
+namespace OIDC.Client.Registration;
+
+public interface IRegistrationService
+{
+    Task<RegistrationResponse> Register(OpenIdConnectOptions options, CancellationToken cancellationToken = default);
+}
