9f6134d74c71a34e9a8764d1d2781e17a540e34f
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2023-06-04 15:02:43 +0200

    update: diagram to use random id for all diagrams

--- Documentation/src/components/Diagram.svelte
+++ Documentation/src/components/Diagram.svelte
@@ -1,11 +1,27 @@
-<script>
+<script lang=ts>
     import { onMount } from 'svelte'
     import mermaid from 'mermaid'
+    let id = makeId();
     onMount(async () => {
-        await mermaid.run();
+        await mermaid.run({
+            nodes: [document.getElementById(id)!],
+        });
     });
+
+    function makeId() {
+        const length = 32;
+        let result = '';
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        const charactersLength = characters.length;
+        let counter = 0;
+        while (counter < length) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+            counter += 1;
+        }
+        return result;
+    }
 </script>
 
-<pre class="mermaid">
+<pre id={id}>
     <slot/>
 </pre>
