4d3a1823666ef7822b8ecd873df8d910941da68c
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2023-06-05 16:44:02 +0200

    remove: tokenbuilder and tokendecoder

--- Authorization/Infrastructure/Decoders/Abstractions/ITokenDecoder.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.IdentityModel.Tokens.Jwt;
-
-namespace Infrastructure.Decoders.Abstractions;
-public interface ITokenDecoder
-{
-  JwtSecurityToken? DecodeSignedToken(string token);
-  JwtSecurityToken? DecodeEncryptedToken(string token);
-}
