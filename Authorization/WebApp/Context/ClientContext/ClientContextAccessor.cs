87a3aa74fb0afa7d7b82fbf1b1e057c4ca2f8d91
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2023-06-05 16:43:13 +0200

    add: client context accessor

---
+++ Authorization/WebApp/Context/ClientContext/ClientContextAccessor.cs
@@ -0,0 +1,12 @@
+﻿using WebApp.Context.Abstract;
+
+namespace WebApp.Context.ClientContext;
+
+public class ClientContextAccessor : IContextAccessor<ClientContext>
+{
+  public async Task<ClientContext> GetContext(HttpContext httpContext)
+  {
+    return await httpContext.Request.ReadFromJsonAsync<ClientContext>()
+      ?? new ClientContext();
+  }
+}
