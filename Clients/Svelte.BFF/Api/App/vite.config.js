a3a8a3d54da85545af8413103c517909d15f6a41
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2023-08-20 20:09:49 +0200

    remove: svelte client

--- Clients/Svelte.BFF/Api/App/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-
-export default defineConfig({
-  plugins: [svelte()],
-  build: {
-    outDir: '../wwwroot/'
-  }
-})
