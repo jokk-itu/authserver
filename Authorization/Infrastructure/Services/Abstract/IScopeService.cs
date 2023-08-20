917fb70e12ce95bf0e6380e1fe24cd482393bcd0
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2023-08-20 20:18:11 +0200

    add: scope service

---
+++ Authorization/Infrastructure/Services/Abstract/IScopeService.cs
@@ -0,0 +1,7 @@
+﻿using Application.Validation;
+
+namespace Infrastructure.Services.Abstract;
+public interface IScopeService
+{
+  Task<BaseValidationResult> ValidateScope(string scope, CancellationToken cancellationToken);
+}
