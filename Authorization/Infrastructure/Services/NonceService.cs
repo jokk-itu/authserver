a26d131037e9dfd94c45e771354a0a7d672e7a18
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2023-08-20 20:18:29 +0200

    add: nonce service

---
+++ Authorization/Infrastructure/Services/NonceService.cs
@@ -0,0 +1,39 @@
+﻿using Application;
+using Application.Validation;
+using Domain;
+using Infrastructure.Services.Abstract;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Services;
+public class NonceService : INonceService
+{
+  private readonly IdentityContext _identityContext;
+
+  public NonceService(
+    IdentityContext identityContext)
+  {
+    _identityContext = identityContext;
+  }
+
+  public async Task<BaseValidationResult> ValidateNonce(string nonce, CancellationToken cancellationToken)
+  {
+    if (string.IsNullOrWhiteSpace(nonce))
+    {
+      return new BaseValidationResult(ErrorCode.InvalidRequest, "nonce is invalid");
+    }
+
+    var isNonceNotUnique = await _identityContext
+      .Set<Nonce>()
+      .Where(x => x.Value == nonce)
+      .AnyAsync(cancellationToken: cancellationToken);
+
+    if (isNonceNotUnique)
+    {
+      return new BaseValidationResult(
+        ErrorCode.InvalidRequest,
+        "nonce is not unique");
+    }
+
+    return new BaseValidationResult();
+  }
+}
---
+++ Authorization/Specs/Services/NonceServiceTests.cs
@@ -0,0 +1,64 @@
+﻿using Application;
+using Infrastructure.Helpers;
+using Infrastructure.Services.Abstract;
+using Microsoft.Extensions.DependencyInjection;
+using Specs.Helpers.EntityBuilders;
+using Xunit;
+
+namespace Specs.Services;
+
+public class NonceServiceTests : BaseUnitTest
+{
+  [Theory]
+  [InlineData("")]
+  [InlineData(null)]
+  public async Task ValidateNonce_NullAndEmptyNonce_InvalidRequest(string nonce)
+  {
+    // Arrange
+    var serviceProvider = BuildServiceProvider();
+    var service = serviceProvider.GetRequiredService<INonceService>();
+
+    // Act
+    var validationResult = await service.ValidateNonce(nonce, CancellationToken.None);
+
+    // Assert
+    Assert.True(validationResult.IsError());
+    Assert.Equal(ErrorCode.InvalidRequest, validationResult.ErrorCode);
+  }
+
+  [Fact]
+  public async Task ValidateNonce_NonUniqueNonce_InvalidRequest()
+  {
+    // Arrange
+    var serviceProvider = BuildServiceProvider();
+    var service = serviceProvider.GetRequiredService<INonceService>();
+
+    var nonce = NonceBuilder
+      .Instance(CryptographyHelper.GetRandomString(16))
+      .Build();
+
+    await IdentityContext.AddAsync(nonce);
+    await IdentityContext.SaveChangesAsync();
+
+    // Act
+    var validationResult = await service.ValidateNonce(nonce.Value, CancellationToken.None);
+
+    // Assert
+    Assert.True(validationResult.IsError());
+    Assert.Equal(ErrorCode.InvalidRequest, validationResult.ErrorCode);
+  }
+
+  [Fact]
+  public async Task ValidateNonce_Ok()
+  {
+    // Arrange
+    var serviceProvider = BuildServiceProvider();
+    var service = serviceProvider.GetRequiredService<INonceService>();
+
+    // Act
+    var validationResult = await service.ValidateNonce(Guid.NewGuid().ToString(), CancellationToken.None);
+
+    // Assert
+    Assert.False(validationResult.IsError());
+  }
+}
