6727c2e1080f25ec81bdaebcca54869504404924
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2023-08-20 20:11:24 +0200

    add: select_account prompt

--- Authorization/Domain/Constants/PromptConstants.cs
+++ Authorization/Domain/Constants/PromptConstants.cs
@@ -3,8 +3,15 @@ public static class PromptConstants
 {
   public const string Login = "login";
   public const string Consent = "consent";
-  public const string Create = "create";
   public const string None = "none";
+  public const string SelectAccount = "select_account";
 
-  public static readonly string[] Prompts = { $"{Consent} {Login}" ,$"{Login} {Consent}", Create, Login, None, Consent };
+  public static readonly string[] Prompts =
+  {
+      $"{Consent} {Login}",
+      $"{Login} {Consent}",
+      Login,
+      None,
+      SelectAccount
+  };
 }
