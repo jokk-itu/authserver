cbc76c42a5b9ba9e7fbf5f58409b28d3d85b0679
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2023-09-02 19:44:44 +0200

    add: issued at migration in nonce

--- Authorization/Domain/Nonce.cs
+++ Authorization/Domain/Nonce.cs
@@ -5,5 +5,6 @@ public class Nonce
 {
   public string Id { get; set; } = Guid.NewGuid().ToString();
   public string Value { get; set; }
+  public DateTime IssuedAt { get; set; }
   public AuthorizationCodeGrant AuthorizationCodeGrant { get; set; }
 }
--- Authorization/Infrastructure/Migrations/20230902173045_AddIssuedAtColumnInNonce.Designer.cs
+++ Authorization/Infrastructure/Migrations/20230902173045_AddIssuedAtColumnInNonce.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(IdentityContext))]
-    partial class IdentityContextModelSnapshot : ModelSnapshot
+    [Migration("20230902173045_AddIssuedAtColumnInNonce")]
+    partial class AddIssuedAtColumnInNonce
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.10");
