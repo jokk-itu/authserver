f740cd16350a00b934d26bed24c351b79bab2bcd
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2023-09-09 13:37:31 +0200

    chore: cleanup unused parameters

--- Authorization/Specs/BaseUnitTest.cs
+++ Authorization/Specs/BaseUnitTest.cs
@@ -1,4 +1,4 @@
-using Application;
+﻿using Application;
 using Infrastructure;
 using Infrastructure.Extensions;
 using Infrastructure.Helpers;
@@ -41,8 +41,6 @@ public abstract class BaseUnitTest
         AccessTokenExpiration = 3600,
         IdTokenExpiration = 300,
         RefreshTokenExpiration = 26000,
-        EncryptingKeySecret = "WnZr4u7w!z%C*F-J",
-        CodeSecret = CryptographyHelper.GetRandomString(32),
         PrivateKeySecret = CryptographyHelper.GetRandomString(32),
         ServiceDocumentation = "http://localhost:5173",
         Issuer = "auth-server",
