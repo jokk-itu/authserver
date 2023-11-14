fe686ca25af7577be812af8a1941f462906653cb
Joachim Køcher Kelsen <jkke@netcompany.com>
2023-11-14 23:37:09 +0100

    add: uri column for resource entity

---
+++ Authorization/Infrastructure/Services/Abstract/IResourceService.cs
@@ -0,0 +1,8 @@
+﻿using Application.Validation;
+
+namespace Infrastructure.Services.Abstract;
+
+public interface IResourceService
+{
+    Task<BaseValidationResult> ValidateResources(ICollection<string> resource, string scope);
+}
