d79eedb9d8d104323d67a1da399d382e52a8f9bd
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2023-12-10 22:08:12 +0100

    add: client authentication

---
+++ Authorization/Infrastructure/Requests/Abstract/ClientAuthentication.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Enums;
+
+namespace Infrastructure.Requests.Abstract;
+
+#nullable disable
+public class ClientAuthentication
+{
+  public TokenEndpointAuthMethod Method { get; set; }
+  public string ClientId { get; set; }
+  public string ClientSecret { get; set; }
+  public string ClientAssertion { get; set; }
+  public string ClientAssertionType { get; set; }
+}
