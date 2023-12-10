3697ec125937b66870478963ccead82e1ac5e9bd
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2023-12-10 22:09:28 +0100

    add: extension functions for client secrets fetching

---
+++ Authorization/WebApp/Extensions/FormCollectionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Enums;
+using Infrastructure.Requests.Abstract;
+using WebApp.Constants;
+
+namespace WebApp.Extensions;
+
+public static class FormCollectionExtensions
+{
+  public static ClientAuthentication? GetClientSecretPost(this IFormCollection formCollection)
+  {
+    var clientAuthentication = new ClientAuthentication
+    {
+      Method = TokenEndpointAuthMethod.ClientSecretPost
+    };
+
+    if (formCollection.TryGetValue(ParameterNames.ClientId, out var clientId))
+    {
+      clientAuthentication.ClientId = clientId;
+    }
+
+    if (formCollection.TryGetValue(ParameterNames.ClientSecret, out var clientSecret))
+    {
+      clientAuthentication.ClientSecret = clientSecret;
+    }
+
+    var isClientSecretPost = !string.IsNullOrWhiteSpace(clientId) || !string.IsNullOrWhiteSpace(clientSecret);
+    return isClientSecretPost ? clientAuthentication : null;
+  }
+}
---
+++ Authorization/WebApp/Extensions/HttpContextExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Enums;
+using Infrastructure.Requests.Abstract;
+using Microsoft.AspNetCore.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace WebApp.Extensions;
+
+public static class HttpContextExtensions
+{
+  public static ClientAuthentication? GetClientSecretBasic(this HttpContext context)
+  {
+    var clientAuthentication = new ClientAuthentication
+    {
+      Method = TokenEndpointAuthMethod.ClientSecretBasic
+    };
+
+    var isBasicAuthentication =
+      AuthenticationHeaderValue.TryParse(context.Request.Headers.Authorization, out var authenticationHeader);
+
+    if (isBasicAuthentication
+        && authenticationHeader!.Scheme == "Basic"
+        && !string.IsNullOrWhiteSpace(authenticationHeader.Parameter))
+    {
+      var decodedBytes = Convert.FromBase64String(authenticationHeader.Parameter);
+      var decoded = Encoding.UTF8.GetString(decodedBytes).Split(":");
+
+      var clientId = decoded.ElementAtOrDefault(0);
+      var clientSecret = decoded.ElementAtOrDefault(1);
+      clientAuthentication.ClientId = clientId;
+      clientAuthentication.ClientSecret = clientSecret;
+
+      var isClientSecretBasic = !string.IsNullOrWhiteSpace(clientId) || !string.IsNullOrWhiteSpace(clientSecret);
+      return isClientSecretBasic ? clientAuthentication : null;
+    }
+  }
