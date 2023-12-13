2eb5aae6f9aae586e7c07713a7cea5e5c9f709dd
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2023-12-13 22:56:46 +0100

    update: get client secret basic

--- Authorization/WebApp/Extensions/HttpContextExtensions.cs
+++ Authorization/WebApp/Extensions/HttpContextExtensions.cs
@@ -1,6 +1,5 @@
 ﻿using Domain.Enums;
 using Infrastructure.Requests.Abstract;
-using Microsoft.AspNetCore.Http;
 using System.Net.Http.Headers;
 using System.Text;
 
@@ -10,27 +9,27 @@ public static class HttpContextExtensions
 {
   public static ClientAuthentication? GetClientSecretBasic(this HttpContext context)
   {
+    if (!AuthenticationHeaderValue.TryParse(context.Request.Headers.Authorization, out var authenticationHeader)
+        || authenticationHeader.Scheme != "Basic"
+        || string.IsNullOrWhiteSpace(authenticationHeader.Parameter))
+    {
+      return null;
+    }
+
+    var decodedBytes = Convert.FromBase64String(authenticationHeader.Parameter);
+    var decoded = Encoding.UTF8.GetString(decodedBytes).Split(":");
+
+    var clientId = decoded.ElementAtOrDefault(0);
+    var clientSecret = decoded.ElementAtOrDefault(1);
+
     var clientAuthentication = new ClientAuthentication
     {
-      Method = TokenEndpointAuthMethod.ClientSecretBasic
+        Method = TokenEndpointAuthMethod.ClientSecretBasic,
+        ClientId = clientId,
+        ClientSecret = clientSecret
     };
 
-    var isBasicAuthentication =
-      AuthenticationHeaderValue.TryParse(context.Request.Headers.Authorization, out var authenticationHeader);
-
-    if (isBasicAuthentication
-        && authenticationHeader!.Scheme == "Basic"
-        && !string.IsNullOrWhiteSpace(authenticationHeader.Parameter))
-    {
-      var decodedBytes = Convert.FromBase64String(authenticationHeader.Parameter);
-      var decoded = Encoding.UTF8.GetString(decodedBytes).Split(":");
-
-      var clientId = decoded.ElementAtOrDefault(0);
-      var clientSecret = decoded.ElementAtOrDefault(1);
-      clientAuthentication.ClientId = clientId;
-      clientAuthentication.ClientSecret = clientSecret;
-
-      var isClientSecretBasic = !string.IsNullOrWhiteSpace(clientId) || !string.IsNullOrWhiteSpace(clientSecret);
-      return isClientSecretBasic ? clientAuthentication : null;
-    }
+    var isClientSecretBasic = !string.IsNullOrWhiteSpace(clientId) || !string.IsNullOrWhiteSpace(clientSecret);
+    return isClientSecretBasic ? clientAuthentication : null;
   }
+}
