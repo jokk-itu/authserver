e7825d4be360e513f285b61686428ada7c0be66f
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2024-01-13 10:23:49 +0100

    update: make claims conditional to only allow values

--- Authorization/Infrastructure/Services/ClaimService.cs
+++ Authorization/Infrastructure/Services/ClaimService.cs
@@ -47,31 +47,31 @@ public class ClaimService : IClaimService
     {
       switch (claim)
       {
-        case ClaimNameConstants.Name:
+        case ClaimNameConstants.Name when !string.IsNullOrWhiteSpace(query.User.GetName()):
           userInfo.Add(claim, query.User.GetName());
           break;
-        case ClaimNameConstants.GivenName:
+        case ClaimNameConstants.GivenName when !string.IsNullOrWhiteSpace(query.User.FirstName):
           userInfo.Add(claim, query.User.FirstName);
           break;
-        case ClaimNameConstants.FamilyName:
+        case ClaimNameConstants.FamilyName when !string.IsNullOrWhiteSpace(query.User.LastName):
           userInfo.Add(claim, query.User.LastName);
           break;
-        case ClaimNameConstants.Address:
+        case ClaimNameConstants.Address when !string.IsNullOrWhiteSpace(query.User.Address):
           userInfo.Add(claim, query.User.Address);
           break;
         case ClaimNameConstants.Birthdate:
           userInfo.Add(claim, query.User.Birthdate.ToString(CultureInfo.InvariantCulture));
           break;
-        case ClaimNameConstants.Locale:
+        case ClaimNameConstants.Locale when !string.IsNullOrWhiteSpace(query.User.Locale):
           userInfo.Add(claim, query.User.Locale);
           break;
-        case ClaimNameConstants.Phone:
+        case ClaimNameConstants.Phone when !string.IsNullOrWhiteSpace(query.User.PhoneNumber):
           userInfo.Add(claim, query.User.PhoneNumber);
           break;
-        case ClaimNameConstants.Email:
+        case ClaimNameConstants.Email when !string.IsNullOrWhiteSpace(query.User.Email):
           userInfo.Add(claim, query.User.Email);
           break;
-        case ClaimNameConstants.Role:
+        case ClaimNameConstants.Role when query.User.Roles.Any():
           userInfo.Add(claim, query.User.Roles.Select(r => r.Value));
           break;
       }
