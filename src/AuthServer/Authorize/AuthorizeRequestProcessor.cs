1bab755018ca8aacd691ca899a8f182578d0b9d1
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2024-05-26 20:03:50 +0200

    add: authorize request processor for authorize

---
+++ src/AuthServer/Authorize/AuthorizeRequestProcessor.cs
@@ -0,0 +1,29 @@
+﻿using AuthServer.Authorize.Abstract;
+using AuthServer.Core.RequestProcessing;
+using AuthServer.RequestAccessors.Authorize;
+
+namespace AuthServer.Authorize;
+
+internal class AuthorizeRequestProcessor : RequestProcessor<AuthorizeRequest, AuthorizeValidatedRequest, string>
+{
+    private readonly IRequestValidator<AuthorizeRequest, AuthorizeValidatedRequest> _requestValidator;
+    private readonly IAuthorizeProcessor _authorizeProcessor;
+
+    public AuthorizeRequestProcessor(
+        IRequestValidator<AuthorizeRequest, AuthorizeValidatedRequest> requestValidator,
+        IAuthorizeProcessor authorizeProcessor)
+    {
+        _requestValidator = requestValidator;
+        _authorizeProcessor = authorizeProcessor;
+    }
+
+    protected override async Task<ProcessResult<string, ProcessError>> ProcessRequest(AuthorizeValidatedRequest request, CancellationToken cancellationToken)
+    {
+        return await _authorizeProcessor.Process(request, cancellationToken);
+    }
+
+    protected override async Task<ProcessResult<AuthorizeValidatedRequest, ProcessError>> ValidateRequest(AuthorizeRequest request, CancellationToken cancellationToken)
+    {
+        return await _requestValidator.Validate(request, cancellationToken);
+    }
+}
