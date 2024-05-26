2d5c782dfc489607bb656187a03ed7a0cd5f3bd2
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2024-05-26 19:52:22 +0200

    remove: query and fragment in response modes

--- src/AuthServer/Constants/ResponseModeConstants.cs
+++ src/AuthServer/Constants/ResponseModeConstants.cs
@@ -2,13 +2,9 @@
 
 public static class ResponseModeConstants
 {
-    public const string Query = "query";
-    public const string Fragment = "fragment";
     public const string FormPost = "form_post";
-    public const string QueryJwt = "query.jwt";
-    public const string FragmentJwt = "fragment.jwt";
     public const string FormPostJwt = "form_post.jwt";
     public const string Jwt = "jwt";
 
-    public static readonly string[] ResponseModes = [Query, Fragment, FormPost, QueryJwt, FragmentJwt, FormPostJwt, Jwt];
+    public static readonly string[] ResponseModes = [FormPost, FormPostJwt, Jwt];
 }
