a26db6d4f4f28727deec1af0aea09cf135fd54a7
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2024-05-26 19:51:18 +0200

    rremove: create prompt as it requires id_token in frontchannel on success

--- src/AuthServer/Constants/PromptConstants.cs
+++ src/AuthServer/Constants/PromptConstants.cs
@@ -6,8 +6,7 @@ public static class PromptConstants
     public const string Consent = "consent";
     public const string None = "none";
     public const string SelectAccount = "select_account";
-    public const string Create = "create";
 
     public static readonly string[] Prompts =
-        [Login, None, SelectAccount, Consent, Create];
+        [Login, None, SelectAccount, Consent];
 }
