1acc3a148cd40fc9dbf1bc0aa1acd218d994eea6
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2024-05-26 20:01:21 +0200

    add: authorization grant repository for new grants

---
+++ src/AuthServer/Repositories/Abstract/IAuthorizationGrantRepository.cs
@@ -0,0 +1,16 @@
+﻿using AuthServer.Entities;
+
+namespace AuthServer.Repositories.Abstract;
+
+internal interface IAuthorizationGrantRepository
+{
+    /// <summary>
+    /// Creates a new grant and revoked the previous grant, if it exists.
+    /// </summary>
+    /// <param name="subjectIdentifier"></param>
+    /// <param name="clientId"></param>
+    /// <param name="maxAge"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task<AuthorizationGrant> CreateAuthorizationGrant(string subjectIdentifier, string clientId, long? maxAge, CancellationToken cancellationToken);
+}
---
+++ src/AuthServer/Repositories/AuthorizationGrantRepository.cs
@@ -0,0 +1,57 @@
+﻿using AuthServer.Core;
+using AuthServer.Entities;
+using AuthServer.Repositories.Abstract;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthServer.Repositories;
+
+internal class AuthorizationGrantRepository : IAuthorizationGrantRepository
+{
+    private readonly IdentityContext _identityContext;
+
+    public AuthorizationGrantRepository(
+        IdentityContext identityContext)
+    {
+        _identityContext = identityContext;
+    }
+
+    /// <inheritdoc/>
+    public async Task<AuthorizationGrant> CreateAuthorizationGrant(string subjectIdentifier, string clientId, long? maxAge,
+        CancellationToken cancellationToken)
+    {
+        var session = await _identityContext
+            .Set<Session>()
+            .Include(x => x.PublicSubjectIdentifier)
+            .Where(x => x.PublicSubjectIdentifier.Id == subjectIdentifier)
+            .Where(x => x.RevokedAt == null)
+            .SingleOrDefaultAsync(cancellationToken);
+
+        var publicSubjectIdentifier = session?.PublicSubjectIdentifier
+                                      ?? await _identityContext
+                                          .Set<PublicSubjectIdentifier>()
+                                          .Where(x => x.Id == subjectIdentifier)
+                                          .SingleAsync(cancellationToken);
+
+        session ??= new Session(publicSubjectIdentifier);
+
+        var grant = await _identityContext
+            .Set<AuthorizationGrant>()
+            .Include(x => x.Client)
+            .Where(x => x.Client.Id == clientId)
+            .Where(x => x.Session.RevokedAt == null)
+            .Where(x => x.Session.PublicSubjectIdentifier.Id == subjectIdentifier)
+            .SingleOrDefaultAsync(cancellationToken);
+
+        grant?.Revoke();
+
+        var client = grant?.Client
+                     ?? await _identityContext
+                         .Set<Client>()
+                         .Where(x => x.Id == clientId)
+                         .SingleAsync(cancellationToken);
+
+        var newGrant = new AuthorizationGrant(DateTime.UtcNow, session, client, publicSubjectIdentifier, maxAge);
+        await _identityContext.AddAsync(newGrant, cancellationToken);
+        return newGrant;
+    }
+}
