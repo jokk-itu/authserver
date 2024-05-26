b285c236b441de7ea46e0f56beb64ce7ddfee40a
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2024-05-26 20:00:15 +0200

    add: user accessor for logged in user

---
+++ src/AuthServer/Authorize/Abstract/IUserAccessor.cs
@@ -0,0 +1,35 @@
+﻿namespace AuthServer.Authorize.Abstract;
+
+internal interface IUserAccessor
+{
+    /// <summary>
+    /// Gets the authenticated user.
+    ///
+    /// Throws an exception if it is not set.
+    /// </summary>
+    /// <returns>authenticated user</returns>
+    User GetUser();
+
+    /// <summary>
+    /// Gets the authenticated user if it exists.
+    /// </summary>
+    /// <returns>authenticated user or null</returns>
+    User? TryGetUser();
+
+    /// <summary>
+    /// Sets an authenticated user.
+    ///
+    /// Throws an exception if a user has already been set.
+    /// </summary>
+    /// <param name="user"></param>
+    void SetUser(User user);
+
+    /// <summary>
+    /// Tries to set an authenticated user.
+    /// </summary>
+    /// <param name="user"></param>
+    /// <returns>true if the user is set, false otherwise.</returns>
+    bool TrySetUser(User user);
+}
+
+public record User(string SubjectIdentifier, IEnumerable<string> Amr);
---
+++ src/AuthServer/Authorize/UserAccessor.cs
@@ -0,0 +1,26 @@
+﻿using AuthServer.Authorize.Abstract;
+
+namespace AuthServer.Authorize;
+
+internal class UserAccessor : IUserAccessor
+{
+    public User GetUser()
+    {
+        throw new NotImplementedException();
+    }
+
+    public User? TryGetUser()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SetUser(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool TrySetUser(User user)
+    {
+        throw new NotImplementedException();
+    }
+}
