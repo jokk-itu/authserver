e1ec6271fb84b0f03c0c8efae0443f21b8ac1229
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2024-06-06 21:49:58 +0200

    add: tests for introspection request accessor

---
+++ tests/AuthServer.Tests.Unit/RequestAccessors/IntrospectionRequesterAccessorTest.cs
@@ -0,0 +1,85 @@
+﻿using System.Text;
+using AuthServer.Core;
+using AuthServer.RequestAccessors.Introspection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Primitives;
+using Xunit.Abstractions;
+
+namespace AuthServer.Tests.Unit.RequestAccessors;
+
+public class IntrospectionRequesterAccessorTest(ITestOutputHelper outputHelper) : BaseUnitTest(outputHelper)
+{
+	[Theory]
+	[InlineData("", "")]
+	[InlineData(null, "")]
+	public async Task GetRequest_NormalStringParameters_ExpectNoValues(string? value, string expectedValue)
+	{
+		// Arrange
+		var serviceProvider = BuildServiceProvider();
+		var requestAccessor = serviceProvider.GetRequiredService<IRequestAccessor<IntrospectionRequest>>();
+		var formUrlContent = new Dictionary<string, StringValues>
+		{
+			{ Parameter.Token, value },
+			{ Parameter.TokenTypeHint, value },
+			{ Parameter.ClientId, value },
+			{ Parameter.ClientSecret, value },
+			{ Parameter.ClientAssertion, value },
+			{ Parameter.ClientAssertionType, value },
+		};
+
+		var httpContext = new DefaultHttpContext
+		{
+			Request =
+			{
+				Form = new FormCollection(formUrlContent),
+			}
+		};
+
+		// Act
+		var request = await requestAccessor.GetRequest(httpContext.Request);
+
+		// Assert
+		Assert.Equal(expectedValue, request.Token);
+		Assert.Equal(expectedValue, request.TokenTypeHint);
+		Assert.Empty(request.ClientAuthentications);
+	}
+
+	[Theory]
+	[InlineData("random_value", "random_value")]
+	public async Task GetRequest_NormalStringParameters_ExpectValues(string? value, string expectedValue)
+	{
+		// Arrange
+		var serviceProvider = BuildServiceProvider();
+		var requestAccessor = serviceProvider.GetRequiredService<IRequestAccessor<IntrospectionRequest>>();
+		var formUrlContent = new Dictionary<string, StringValues>
+		{
+			{ Parameter.Token, value },
+			{ Parameter.TokenTypeHint, value },
+			{ Parameter.ClientId, value },
+			{ Parameter.ClientSecret, value },
+			{ Parameter.ClientAssertion, value },
+			{ Parameter.ClientAssertionType, value },
+		};
+
+		var httpContext = new DefaultHttpContext
+		{
+			Request =
+			{
+				Form = new FormCollection(formUrlContent),
+				Headers =
+				{
+					Authorization = $"Basic {Convert.ToBase64String(Encoding.UTF8.GetBytes($"{value}:{value}"))}"
+				}
+			}
+		};
+
+		// Act
+		var request = await requestAccessor.GetRequest(httpContext.Request);
+
+		// Assert
+		Assert.Equal(expectedValue, request.Token);
+		Assert.Equal(expectedValue, request.TokenTypeHint);
+		Assert.Equal(3, request.ClientAuthentications.Count);
+	}
+}
