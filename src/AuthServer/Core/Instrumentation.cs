f5fcf4e9e9a47eb7b3d00c188af724a1532bf181
NCLAN\jkke <jkke@netcompany.com>
2024-06-24 22:31:37 +0200

    add: static instrumentation with activitysource for diagnostics

---
+++ src/AuthServer/Core/Instrumentation.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.Metrics;
+
+namespace AuthServer.Core;
+
+internal static class Instrumentation
+{
+    public static readonly ActivitySource ActivitySource = new("AuthServer");
+
+    // TODO Tokens built
+    // TODO Tokens revoked
+    // TODO Tokens fetched
+
+    // TODO Created clients
+    // TODO Updated clients
+    // TODO Fetched clients
+    // TODO Deleted clients
+
+    // TODO Fetched Userinfo
+
+    // TODO Authorize invoked (prompt, client_id)
+}
