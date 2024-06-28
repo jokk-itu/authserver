006e1e3c0fb55363cbff96cc5d5d0ccb014c198e
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2024-06-29 00:01:24 +0200

    add: get register handler

---
+++ src/AuthServer/Register/GetClient/GetRegisterRequestValidator.cs
@@ -0,0 +1,47 @@
+﻿using AuthServer.Core.Request;
+using AuthServer.Repositories.Abstractions;
+using AuthServer.RequestAccessors.Register;
+
+namespace AuthServer.Register.GetClient;
+
+internal class GetRegisterRequestValidator : IRequestValidator<GetRegisterRequest, GetRegisterValidatedRequest>
+{
+	private readonly ITokenRepository _tokenRepository;
+
+	public GetRegisterRequestValidator(ITokenRepository tokenRepository)
+	{
+		_tokenRepository = tokenRepository;
+	}
+
+	public async Task<ProcessResult<GetRegisterValidatedRequest, ProcessError>> Validate(GetRegisterRequest request, CancellationToken cancellationToken)
+	{
+		/* ClientId is REQUIRED */
+		var clientId = request.ClientId;
+		if (string.IsNullOrEmpty(clientId))
+		{
+			// TODO invalid ClientId given
+		}
+
+		var registrationAccessToken = request.RegistrationAccessToken;
+		if (string.IsNullOrEmpty(registrationAccessToken))
+		{
+			// TODO invalid RegistrationAccessToken given
+		}
+
+		var token = await _tokenRepository.GetRegistrationToken(registrationAccessToken, cancellationToken);
+		if (token is null)
+		{
+			// TODO invalid RegistrationAccessToken given
+		}
+		else if (token.Client.Id != clientId)
+		{
+			// TODO invalid Client mismatch
+		}
+
+		return new GetRegisterValidatedRequest
+		{
+			ClientId = clientId,
+			RegistrationAccessToken = registrationAccessToken
+		};
+	}
+}
---
+++ src/AuthServer/Register/GetClient/GetRegisterValidatedRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace AuthServer.Register.GetClient;
+
+internal class GetRegisterValidatedRequest
+{
+	public required string ClientId { get; init; }
+	public required string RegistrationAccessToken { get; init; }
+}
