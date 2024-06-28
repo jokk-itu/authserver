b615792c7bc1e00f905990c16d4cf0084fc43cb2
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2024-06-29 00:01:37 +0200

    add: delete register handler

---
+++ src/AuthServer/Register/DeleteClient/DeleteRegisterValidatedRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace AuthServer.Register.DeleteClient;
+
+internal class DeleteRegisterValidatedRequest
+{
+    public required string ClientId { get; init; }
+    public required string RegistrationAccessToken { get; init; }
+}
