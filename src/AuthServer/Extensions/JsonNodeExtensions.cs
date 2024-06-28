4e8ee58ff514c9b920791d2efbc987d5131d5c81
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2024-06-28 23:52:52 +0200

    add: extension function for getting properties from json nodes

---
+++ src/AuthServer/Extensions/JsonNodeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Nodes;
+
+namespace AuthServer.Extensions;
+
+internal static class JsonNodeExtensions
+{
+	public static string GetValueOrEmpty(this JsonNode node, string key)
+	{
+		return node[key]?.GetValue<string>() ?? string.Empty;
+	}
+
+	public static IReadOnlyCollection<string> GetSpaceDelimitedValueOrEmpty(this JsonNode node, string key)
+	{
+		return node[key]?.GetValue<string[]>() ?? [];
+	}
+}
