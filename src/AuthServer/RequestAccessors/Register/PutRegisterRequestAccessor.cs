58f2a6195b3b5fd4174e3e4358d43574fb0a7abe
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2024-06-29 11:12:12 +0200

    update: register request accessors to use extension methods getting values

--- src/AuthServer/RequestAccessors/Register/PutRegisterRequestAccessor.cs
+++ src/AuthServer/RequestAccessors/Register/PutRegisterRequestAccessor.cs
@@ -20,50 +20,50 @@ internal class PutRegisterRequestAccessor : IRequestAccessor<PutRegisterRequest>
 		
 		var clientId = httpRequest.Query.GetValueOrEmpty(Parameter.ClientId);
 		var registrationAccessToken = await httpRequest.HttpContext.GetTokenAsync(JwtBearerDefaults.AuthenticationScheme, Parameter.AccessToken) ?? string.Empty;
+
 		var clientName = json.GetValueOrEmpty(Parameter.ClientName);
-		var applicationType = json[Parameter.ApplicationType]?.GetValue<string>() ?? string.Empty;
-		var subjectType = json[Parameter.Subject]?.GetValue<string>() ?? string.Empty;
-		var defaultMaxAge = json[Parameter.DefaultMaxAge]?.GetValue<string>() ?? string.Empty;
-		var tokenEndpointAuthMethod = json[Parameter.TokenEndpointAuthMethod]?.GetValue<string>() ?? string.Empty;
-		var tokenEndpointAuthSigningAlg = json[Parameter.TokenEndpointAuthSigningAlg]?.GetValue<string>() ?? string.Empty;
-		var jwks = json[Parameter.Jwks]?.GetValue<string>() ?? string.Empty;
-		var jwksUri = json[Parameter.JwksUri]?.GetValue<string>() ?? string.Empty;
-		var clientUri = json[Parameter.ClientUri]?.GetValue<string>() ?? string.Empty;
-		var policyUri = json[Parameter.PolicyUri]?.GetValue<string>() ?? string.Empty;
-		var tosUri = json[Parameter.TosUri]?.GetValue<string>() ?? string.Empty;
-		var initiateLoginUri = json[Parameter.InitiateLoginUri]?.GetValue<string>() ?? string.Empty;
-		var logoUri = json[Parameter.LogoUri]?.GetValue<string>() ?? string.Empty;
-		var backchannelLogoutUri = json[Parameter.BackchannelLogoutUri]?.GetValue<string>() ?? string.Empty;
-		var requireSignedRequestObject = json[Parameter.RequireSignedRequestObject]?.GetValue<string>() ?? string.Empty;
-		var requireReferenceToken = json[Parameter.RequireReferenceToken]?.GetValue<string>() ?? string.Empty;
-		var requestObjectEncryptionEnc = json[Parameter.RequestObjectEncryptionEnc]?.GetValue<string>() ?? string.Empty;
-		var requestObjectEncryptionAlg = json[Parameter.RequestObjectEncryptionAlg]?.GetValue<string>() ?? string.Empty;
-		var requestObjectSigningAlg = json[Parameter.RequestObjectSigningAlg]?.GetValue<string>() ?? string.Empty;
-		var userinfoEncryptedResponseEnc = json[Parameter.UserinfoEncryptedResponseEnc]?.GetValue<string>() ?? string.Empty;
-		var userinfoEncryptedResponseAlg = json[Parameter.UserinfoEncryptedResponseAlg]?.GetValue<string>() ?? string.Empty;
-		var userinfoSignedResponseAlg = json[Parameter.UserinfoSignedResponseAlg]?.GetValue<string>() ?? string.Empty;
-		var idTokenEncryptedResponseEnc = json[Parameter.IdTokenEncryptedResponseEnc]?.GetValue<string>() ?? string.Empty;
-		var idTokenEncryptedResponseAlg = json[Parameter.IdTokenEncryptedResponseAlg]?.GetValue<string>() ?? string.Empty;
-		var idTokenSignedResponseAlg = json[Parameter.IdTokenSignedResponseAlg]?.GetValue<string>() ?? string.Empty;
-		var authorizationEncryptedResponseEnc = json[Parameter.AuthorizationEncryptedResponseEnc]?.GetValue<string>() ?? string.Empty;
-		var authorizationEncryptedResponseAlg = json[Parameter.AuthorizationEncryptedResponseAlg]?.GetValue<string>() ?? string.Empty;
-		var authorizationSignedResponseAlg = json[Parameter.AuthorizationSignedResponseAlg]?.GetValue<string>() ?? string.Empty;
+		var applicationType = json.GetValueOrEmpty(Parameter.ApplicationType);
+		var subjectType = json.GetValueOrEmpty(Parameter.Subject);
+		var defaultMaxAge = json.GetValueOrEmpty(Parameter.DefaultMaxAge);
+		var tokenEndpointAuthMethod = json.GetValueOrEmpty(Parameter.TokenEndpointAuthMethod);
+		var tokenEndpointAuthSigningAlg = json.GetValueOrEmpty(Parameter.TokenEndpointAuthSigningAlg);
+		var jwks = json.GetValueOrEmpty(Parameter.Jwks);
+		var jwksUri = json.GetValueOrEmpty(Parameter.JwksUri);
+		var clientUri = json.GetValueOrEmpty(Parameter.ClientUri);
+		var policyUri = json.GetValueOrEmpty(Parameter.PolicyUri);
+		var tosUri = json.GetValueOrEmpty(Parameter.TosUri);
+		var initiateLoginUri = json.GetValueOrEmpty(Parameter.InitiateLoginUri);
+		var logoUri = json.GetValueOrEmpty(Parameter.LogoUri);
+		var backchannelLogoutUri = json.GetValueOrEmpty(Parameter.BackchannelLogoutUri);
+		var requireSignedRequestObject = json.GetValueOrEmpty(Parameter.RequireSignedRequestObject);
+		var requireReferenceToken = json.GetValueOrEmpty(Parameter.RequireReferenceToken);
+		var requestObjectEncryptionEnc = json.GetValueOrEmpty(Parameter.RequestObjectEncryptionEnc);
+		var requestObjectEncryptionAlg = json.GetValueOrEmpty(Parameter.RequestObjectEncryptionAlg);
+		var requestObjectSigningAlg = json.GetValueOrEmpty(Parameter.RequestObjectSigningAlg);
+		var userinfoEncryptedResponseEnc = json.GetValueOrEmpty(Parameter.UserinfoEncryptedResponseEnc);
+		var userinfoEncryptedResponseAlg = json.GetValueOrEmpty(Parameter.UserinfoEncryptedResponseAlg);
+		var userinfoSignedResponseAlg = json.GetValueOrEmpty(Parameter.UserinfoEncryptedResponseAlg);
+		var idTokenEncryptedResponseEnc = json.GetValueOrEmpty(Parameter.IdTokenEncryptedResponseEnc);
+		var idTokenEncryptedResponseAlg = json.GetValueOrEmpty(Parameter.IdTokenEncryptedResponseAlg);
+		var idTokenSignedResponseAlg = json.GetValueOrEmpty(Parameter.IdTokenSignedResponseAlg);
+		var authorizationEncryptedResponseEnc = json.GetValueOrEmpty(Parameter.AuthorizationEncryptedResponseEnc);
+		var authorizationEncryptedResponseAlg = json.GetValueOrEmpty(Parameter.AuthorizationEncryptedResponseAlg);
+		var authorizationSignedResponseAlg = json.GetValueOrEmpty(Parameter.AuthorizationSignedResponseAlg);
 
-		var authorizationCodeExpiration = json[Parameter.AuthorizationCodeExpiration]?.GetValue<string>() ?? string.Empty;
-		var accessTokenExpiration = json[Parameter.AccessTokenExpiration]?.GetValue<string>() ?? string.Empty;
-		var refreshTokenExpiration = json[Parameter.RefreshTokenExpiration]?.GetValue<string>() ?? string.Empty;
-		var clientSecretExpiration = json[Parameter.ClientSecretExpiration]?.GetValue<string>() ?? string.Empty;
-		var jwksExpiration = json[Parameter.JwksExpiration]?.GetValue<string>() ?? string.Empty;
+		var authorizationCodeExpiration = json.GetValueOrEmpty(Parameter.AuthorizationCodeExpiration);
+		var accessTokenExpiration = json.GetValueOrEmpty(Parameter.AccessTokenExpiration);
+		var refreshTokenExpiration = json.GetValueOrEmpty(Parameter.RefreshTokenExpiration);
+		var clientSecretExpiration = json.GetValueOrEmpty(Parameter.ClientSecretExpiration);
+		var jwksExpiration = json.GetValueOrEmpty(Parameter.JwksExpiration);
 
-		var defaultAcrValues = json[Parameter.DefaultAcrValues]?.GetValue<string>()?.Split(' ') ?? [];
-		var scope = json[Parameter.Scope]?.GetValue<string>()?.Split(' ') ?? [];
-
-		var redirectUris = json[Parameter.RedirectUris]?.GetValue<string[]>() ?? [];
-		var postLogoutRedirectUris = json[Parameter.PostLogoutRedirectUris]?.GetValue<string[]>() ?? [];
-		var requestUris = json[Parameter.RequestUris]?.GetValue<string[]>() ?? [];
-		var responseTypes = json[Parameter.ResponseTypes]?.GetValue<string[]>() ?? [];
-		var grantTypes = json[Parameter.GrantTypes]?.GetValue<string[]>() ?? [];
-		var contacts = json[Parameter.Contacts]?.GetValue<string[]>() ?? [];
+		var defaultAcrValues = json.GetSpaceDelimitedValueOrEmpty(Parameter.DefaultAcrValues);
+		var scope = json.GetSpaceDelimitedValueOrEmpty(Parameter.Scope);
+		var redirectUris = json.GetSpaceDelimitedValueOrEmpty(Parameter.RedirectUris);
+		var postLogoutRedirectUris = json.GetSpaceDelimitedValueOrEmpty(Parameter.PostLogoutRedirectUris);
+		var requestUris = json.GetSpaceDelimitedValueOrEmpty(Parameter.RequestUris);
+		var responseTypes = json.GetSpaceDelimitedValueOrEmpty(Parameter.ResponseTypes);
+		var grantTypes = json.GetSpaceDelimitedValueOrEmpty(Parameter.GrantTypes);
+		var contacts = json.GetSpaceDelimitedValueOrEmpty(Parameter.Contacts);
 
 		return new PutRegisterRequest
 		{
