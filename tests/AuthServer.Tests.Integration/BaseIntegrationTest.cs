302c6b8aedb8567244e9f83535430cad304ece41
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2024-07-14 23:14:27 +0200

    add: base integration test

---
+++ tests/AuthServer.Tests.Integration/BaseIntegrationTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit.Abstractions;
+
+namespace AuthServer.Tests.Integration;
+public abstract class BaseIntegrationTest : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+
+    protected readonly ITestOutputHelper TestOutputHelper;
+    protected readonly IServiceProvider ServiceProvider;
+
+    protected BaseIntegrationTest(WebApplicationFactory<Program> factory, ITestOutputHelper testOutputHelper)
+    {
+        _factory = factory;
+        TestOutputHelper = testOutputHelper;
+        ServiceProvider = _factory.Services.CreateScope().ServiceProvider;
+    }
+
+    protected HttpClient GetHttpClient() => _factory.CreateClient(new WebApplicationFactoryClientOptions
+    {
+        AllowAutoRedirect = false
+    });
+}
