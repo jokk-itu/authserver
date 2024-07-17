b6c6952467139b73001a6a0574948ea8cd358bb7
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2024-07-17 09:43:17 +0200

    add: see other redirect oauth functionality

--- src/AuthServer/Extensions/ResultsExtensions.cs
+++ src/AuthServer/Extensions/ResultsExtensions.cs
@@ -5,15 +5,30 @@ using AuthServer.Core;
 using Microsoft.AspNetCore.Http;
 
 namespace AuthServer.Extensions;
+
 public static class ResultsExtensions
 {
     public static IResult OAuthBadRequest(this IResultExtensions _, OAuthError error)
     {
-        return Results.Text(JsonSerializer.Serialize(error), MimeTypeConstants.Json, Encoding.UTF8, StatusCodes.Status400BadRequest);
+        return Results.Text(JsonSerializer.Serialize(error), MimeTypeConstants.Json, Encoding.UTF8,
+            StatusCodes.Status400BadRequest);
     }
 
     public static IResult OAuthOkWithHtml(this IResultExtensions _, string html)
     {
         return Results.Text(html, MimeTypeConstants.Html, Encoding.UTF8, StatusCodes.Status200OK);
+    }
+
+    /// <summary>
+    /// SeeOther redirect 303 status code.
+    /// </summary>
+    /// <param name="_"></param>
+    /// <param name="location"></param>
+    /// <param name="response"></param>
+    /// <returns></returns>
+    public static IResult OAuthSeeOtherRedirect(this IResultExtensions _, string location, HttpResponse response)
+    {
+        response.Headers.Location = location;
+        return Results.StatusCode(303);
     }
 }
