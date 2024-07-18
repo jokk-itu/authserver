7fa8aea14f3202dce0870264b8ca981364a7767a
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2024-07-18 16:19:27 +0200

    add: tests for grant repository

---
+++ tests/AuthServer.Tests.Unit/Repositories/AuthorizationGrantRepositoryTest.cs
@@ -0,0 +1,113 @@
+﻿using AuthServer.Entities;
+using AuthServer.Enums;
+using AuthServer.Repositories.Abstractions;
+using AuthServer.RequestAccessors.Register;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit.Abstractions;
+
+namespace AuthServer.Tests.Unit.Repositories;
+public class AuthorizationGrantRepositoryTest : BaseUnitTest
+{
+    public AuthorizationGrantRepositoryTest(ITestOutputHelper outputHelper)
+        : base(outputHelper)
+    {
+    }
+
+    [Fact]
+    public async Task CreateAuthorizationGrant_ActiveSession_ExpectGrant()
+    {
+        // Arrange
+        var serviceProvider = BuildServiceProvider();
+        var authorizationGrantRepository = serviceProvider.GetRequiredService<IAuthorizationGrantRepository>();
+        var publicSubjectIdentifier = new PublicSubjectIdentifier();
+        var session = new Session(publicSubjectIdentifier);
+        var client = new Client("webapp", ApplicationType.Web, TokenEndpointAuthMethod.ClientSecretBasic)
+        {
+            SubjectType = SubjectType.Public
+        };
+        await AddEntity(session);
+        await AddEntity(client);
+
+        // Act
+        var grant = await authorizationGrantRepository.CreateAuthorizationGrant(publicSubjectIdentifier.Id, client.Id, null, CancellationToken.None);
+
+        // Assert
+        Assert.Equal(client, grant.Client);
+        Assert.Equal(session, grant.Session);
+        Assert.Equal(publicSubjectIdentifier, grant.SubjectIdentifier);
+    }
+
+    [Fact]
+    public async Task CreateAuthorizationGrant_ActiveSessionWithPreviousGrant_ExpectGrant()
+    {
+        // Arrange
+        var serviceProvider = BuildServiceProvider();
+        var authorizationGrantRepository = serviceProvider.GetRequiredService<IAuthorizationGrantRepository>();
+        var publicSubjectIdentifier = new PublicSubjectIdentifier();
+        var session = new Session(publicSubjectIdentifier);
+        var client = new Client("webapp", ApplicationType.Web, TokenEndpointAuthMethod.ClientSecretBasic)
+        {
+            SubjectType = SubjectType.Public
+        };
+        var previousGrant = new AuthorizationGrant(DateTime.UtcNow, session, client, publicSubjectIdentifier);
+        await AddEntity(previousGrant);
+
+        // Act
+        var grant = await authorizationGrantRepository.CreateAuthorizationGrant(publicSubjectIdentifier.Id, client.Id, null, CancellationToken.None);
+
+        // Assert
+        Assert.Equal(client, grant.Client);
+        Assert.Equal(session, grant.Session);
+        Assert.Equal(publicSubjectIdentifier, grant.SubjectIdentifier);
+        Assert.NotNull(previousGrant.RevokedAt);
+    }
+
+    [Fact]
+    public async Task CreateAuthorizationGrant_SubjectTypePairwise_ExpectGrant()
+    {
+        // Arrange
+        var serviceProvider = BuildServiceProvider();
+        var authorizationGrantRepository = serviceProvider.GetRequiredService<IAuthorizationGrantRepository>();
+        var publicSubjectIdentifier = new PublicSubjectIdentifier();
+        var client = new Client("webapp", ApplicationType.Web, TokenEndpointAuthMethod.ClientSecretBasic)
+        {
+            SubjectType = SubjectType.Pairwise
+        };
+        await AddEntity(publicSubjectIdentifier);
+        await AddEntity(client);
+
+        // Act
+        var grant = await authorizationGrantRepository.CreateAuthorizationGrant(publicSubjectIdentifier.Id, client.Id, null, CancellationToken.None);
+
+        // Assert
+        Assert.Equal(client, grant.Client);
+        Assert.NotNull(grant.Session);
+        Assert.Equal(publicSubjectIdentifier, grant.Session.PublicSubjectIdentifier);
+        Assert.IsType<PairwiseSubjectIdentifier>(grant.SubjectIdentifier);
+        Assert.Equal(publicSubjectIdentifier, ((PairwiseSubjectIdentifier)grant.SubjectIdentifier).PublicSubjectIdentifier);
+    }
+
+    [Fact]
+    public async Task CreateAuthorizationGrant_ExistingPairwiseSubjectIdentifier_ExpectGrant()
+    {
+        // Arrange
+        var serviceProvider = BuildServiceProvider();
+        var authorizationGrantRepository = serviceProvider.GetRequiredService<IAuthorizationGrantRepository>();
+        var publicSubjectIdentifier = new PublicSubjectIdentifier();
+        var client = new Client("webapp", ApplicationType.Web, TokenEndpointAuthMethod.ClientSecretBasic)
+        {
+            SubjectType = SubjectType.Pairwise
+        };
+        var pairwiseSubjectIdentifier = new PairwiseSubjectIdentifier(client, publicSubjectIdentifier);
+        await AddEntity(pairwiseSubjectIdentifier);
+
+        // Act
+        var grant = await authorizationGrantRepository.CreateAuthorizationGrant(publicSubjectIdentifier.Id, client.Id, null, CancellationToken.None);
+
+        // Assert
+        Assert.Equal(client, grant.Client);
+        Assert.NotNull(grant.Session);
+        Assert.Equal(publicSubjectIdentifier, grant.Session.PublicSubjectIdentifier);
+        Assert.Equal(pairwiseSubjectIdentifier, grant.SubjectIdentifier);
+    }
+}
