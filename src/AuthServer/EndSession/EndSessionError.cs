7e97d42485f4c08a8e187a8d666364315873f363
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2024-08-04 01:46:10 +0200

    add: end session error

---
+++ src/AuthServer/EndSession/EndSessionError.cs
@@ -0,0 +1,25 @@
+﻿using AuthServer.Core;
+using AuthServer.Core.Request;
+
+namespace AuthServer.EndSession;
+internal static class EndSessionError
+{
+    public static readonly ProcessError InvalidIdToken =
+        new(ErrorCode.InvalidRequest, "invalid id_token_hint", ResultCode.BadRequest);
+
+    public static readonly ProcessError MismatchingClientId =
+        new(ErrorCode.InvalidRequest, "client_id does not match id_token_hint", ResultCode.BadRequest);
+
+    public static readonly ProcessError InvalidClientId =
+        new(ErrorCode.InvalidRequest, "invalid client_id", ResultCode.BadRequest);
+
+    public static readonly ProcessError InvalidState =
+        new(ErrorCode.InvalidRequest, "invalid state", ResultCode.BadRequest);
+
+    public static readonly ProcessError InvalidPostLogoutRedirectUri =
+        new(ErrorCode.InvalidRequest, "invalid post_logout_redirect_uri", ResultCode.BadRequest);
+
+    // Custom internal error
+    public static readonly ProcessError InteractionRequired =
+        new(ErrorCode.InteractionRequired, "end-user must interact with logout page", ResultCode.Redirect);
+}
