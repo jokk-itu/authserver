c1c432a64e6c933987d7510e3b08101605fc23df
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2024-08-04 01:46:24 +0200

    add: end user validated request

---
+++ src/AuthServer/EndSession/EndSessionValidatedRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthServer.EndSession;
+internal class EndSessionValidatedRequest
+{
+    public required string? SubjectIdentifier { get; init; }
+    public required string? SessionId { get; init; }
+    public required string? ClientId { get; init; }
+    public required bool LogoutAtIdentityProvider { get; init; }
+}
