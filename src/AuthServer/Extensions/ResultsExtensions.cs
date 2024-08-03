b4c5d0468d5840a3c2cc5c9941cc55ae22efa193
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2024-08-04 01:33:17 +0200

    add: local redirect function as extension method with post and get support

--- src/AuthServer/Extensions/ResultsExtensions.cs
+++ src/AuthServer/Extensions/ResultsExtensions.cs
@@ -1,8 +1,10 @@
 ﻿using System.Text;
 using System.Text.Json;
+using System.Web;
 using AuthServer.Constants;
 using AuthServer.Core;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
 
 namespace AuthServer.Extensions;
 
@@ -19,16 +21,29 @@ public static class ResultsExtensions
         return Results.Text(html, MimeTypeConstants.Html, Encoding.UTF8, StatusCodes.Status200OK);
     }
 
-    /// <summary>
-    /// SeeOther redirect 303 status code.
-    /// </summary>
-    /// <param name="_"></param>
-    /// <param name="location"></param>
-    /// <param name="response"></param>
-    /// <returns></returns>
     public static IResult OAuthSeeOtherRedirect(this IResultExtensions _, string location, HttpResponse response)
     {
         response.Headers.Location = location;
         return Results.StatusCode(303);
+    }
+
+    public static IResult LocalRedirect(this IResultExtensions _, string url, HttpContext httpContext)
+    {
+        var rawUrl = string.Empty;
+        if (httpContext.Request.Method == "GET")
+        {
+            rawUrl = httpContext.Request.GetDisplayUrl();
+        }
+        else if (httpContext.Request.Method == "POST")
+        {
+            var body = httpContext.Request.Form;
+            var baseUrl = httpContext.Request.GetDisplayUrl();
+            var query = new QueryBuilder(body).ToQueryString();
+            rawUrl = baseUrl + query;
+        }
+
+        var returnUrl = HttpUtility.UrlEncode(rawUrl);
+        var location = $"{url}?returnUrl={returnUrl}";
+        return Results.Extensions.OAuthSeeOtherRedirect(location, httpContext.Response);
     }
 }
