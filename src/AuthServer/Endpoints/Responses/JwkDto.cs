95db0019e747ca106b2d8e4630e4058222451286
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2024-08-05 21:55:15 +0200

    add: response contracts for jwks

---
+++ src/AuthServer/Endpoints/Responses/JwkDto.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AuthServer.Endpoints.Responses;
+internal class JwkDto
+{
+    [JsonPropertyName("kty")]
+    public required string KeyType { get; init; }
+
+    [JsonPropertyName("use")]
+    public required string Use { get; init; }
+
+    [JsonPropertyName("kid")]
+    public required string KeyId { get; init; }
+
+    [JsonPropertyName("alg")]
+    public required string Alg { get; init; }
+
+    [JsonPropertyName("key_ops")]
+    public required IEnumerable<string> KeysOps { get; init; }
+
+    [JsonPropertyName("n")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string? Modulus { get; set; }
+
+    [JsonPropertyName("e")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string? Exponent { get; set; }
+
+    [JsonPropertyName("x5t")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string? X509Thumbprint { get; set; }
+
+    [JsonPropertyName("crv")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string? Curve { get; set; }
+
+    [JsonPropertyName("x")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string? X { get; set; }
+
+    [JsonPropertyName("y")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string? Y { get; set; }
+}
