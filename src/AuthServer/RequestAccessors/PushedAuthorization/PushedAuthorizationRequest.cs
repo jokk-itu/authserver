8fedd4e0d2a45e4278beba458747e266873c358b
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2024-09-01 21:51:24 +0200

    add: pushed authorization request accessing

---
+++ src/AuthServer/RequestAccessors/PushedAuthorization/PushedAuthorizationRequest.cs
@@ -0,0 +1,23 @@
+﻿using AuthServer.Core.Models;
+
+namespace AuthServer.RequestAccessors.PushedAuthorization;
+internal class PushedAuthorizationRequest
+{
+    public required string IdTokenHint { get; init; }
+    public required string LoginHint { get; init; }
+    public required string Prompt { get; init; }
+    public required string Display { get; init; }
+    public required string ClientId { get; init; }
+    public required string RedirectUri { get; init; }
+    public required string CodeChallenge { get; init; }
+    public required string CodeChallengeMethod { get; init; }
+    public required string ResponseType { get; init; }
+    public required string Nonce { get; init; }
+    public required string MaxAge { get; init; }
+    public required string State { get; init; }
+    public required string ResponseMode { get; init; }
+    public required string RequestObject { get; init; }
+    public required IReadOnlyCollection<string> Scope { get; init; }
+    public required IReadOnlyCollection<string> AcrValues { get; init; }
+    public required IReadOnlyCollection<ClientAuthentication> ClientAuthentications { get; init; }
+}
---
+++ src/AuthServer/RequestAccessors/PushedAuthorization/PushedAuthorizationRequestAccessor.cs
@@ -0,0 +1,63 @@
+﻿using AuthServer.Core;
+using AuthServer.Core.Abstractions;
+using AuthServer.Core.Models;
+using AuthServer.Extensions;
+using AuthServer.TokenDecoders;
+using Microsoft.AspNetCore.Http;
+
+namespace AuthServer.RequestAccessors.PushedAuthorization;
+internal class PushedAuthorizationRequestAccessor : IRequestAccessor<PushedAuthorizationRequest >
+{
+    public async Task<PushedAuthorizationRequest> GetRequest(HttpRequest httpRequest)
+    {
+        var body = await httpRequest.ReadFormAsync();
+
+        var loginHint = body.GetValueOrEmpty(Parameter.LoginHint);
+        var display = body.GetValueOrEmpty(Parameter.Display);
+        var responseMode = body.GetValueOrEmpty(Parameter.ResponseMode);
+        var maxAge = body.GetValueOrEmpty(Parameter.MaxAge);
+        var clientId = body.GetValueOrEmpty(Parameter.ClientId);
+        var codeChallenge = body.GetValueOrEmpty(Parameter.CodeChallenge);
+        var codeChallengeMethod = body.GetValueOrEmpty(Parameter.CodeChallengeMethod);
+        var redirectUri = body.GetValueOrEmpty(Parameter.RedirectUri);
+        var idTokenHint = body.GetValueOrEmpty(Parameter.IdTokenHint);
+        var prompt = body.GetValueOrEmpty(Parameter.Prompt);
+        var responseType = body.GetValueOrEmpty(Parameter.ResponseType);
+        var nonce = body.GetValueOrEmpty(Parameter.Nonce);
+        var state = body.GetValueOrEmpty(Parameter.State);
+        var requestObject = body.GetValueOrEmpty(Parameter.Request);
+
+        var scope = body.GetSpaceDelimitedValueOrEmpty(Parameter.Scope);
+        var acrValues = body.GetSpaceDelimitedValueOrEmpty(Parameter.AcrValues);
+
+        var clientSecretBasic = httpRequest.GetClientSecretBasic();
+        var clientSecretPost = body.GetClientSecretPost();
+        var clientAssertion = body.GetClientAssertion(ClientTokenAudience.PushedAuthorizeEndpoint);
+
+        var clientAuthentications = new List<ClientAuthentication>();
+        if (clientSecretBasic is not null) clientAuthentications.Add(clientSecretBasic);
+        if (clientSecretPost is not null) clientAuthentications.Add(clientSecretPost);
+        if (clientAssertion is not null) clientAuthentications.Add(clientAssertion);
+
+        return new PushedAuthorizationRequest
+        {
+            IdTokenHint = idTokenHint,
+            LoginHint = loginHint,
+            Prompt = prompt,
+            Display = display,
+            ClientId = clientId,
+            RedirectUri = redirectUri,
+            CodeChallenge = codeChallenge,
+            CodeChallengeMethod = codeChallengeMethod,
+            ResponseType = responseType,
+            Nonce = nonce,
+            MaxAge = maxAge,
+            State = state,
+            ResponseMode = responseMode,
+            RequestObject = requestObject,
+            Scope = scope,
+            AcrValues = acrValues,
+            ClientAuthentications = clientAuthentications.AsReadOnly()
+        };
+    }
+}
