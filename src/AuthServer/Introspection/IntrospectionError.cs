cb6f13ce7bfc254d8f601ab954e6a17dde47e15f
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2024-09-15 11:43:30 +0200

    updare: reformat

--- src/AuthServer/Introspection/IntrospectionError.cs
+++ src/AuthServer/Introspection/IntrospectionError.cs
@@ -4,11 +4,11 @@ using AuthServer.Core.Request;
 namespace AuthServer.Introspection;
 internal class IntrospectionError
 {
-    public static readonly ProcessError UnsupportedTokenType = new(ErrorCode.UnsupportedTokenType,
-        "provided token_type_hint is unsupported", ResultCode.BadRequest);
+    public static readonly ProcessError UnsupportedTokenType =
+        new(ErrorCode.UnsupportedTokenType, "provided token_type_hint is unsupported", ResultCode.BadRequest);
 
-    public static readonly ProcessError EmptyToken = new(ErrorCode.InvalidRequest,
-        "token must not be null or empty", ResultCode.BadRequest);
+    public static readonly ProcessError EmptyToken =
+        new(ErrorCode.InvalidRequest, "token must not be null or empty", ResultCode.BadRequest);
 
     public static readonly ProcessError MultipleOrNoneClientMethod = new(ErrorCode.InvalidClient,
         "only one client authentication method must be used", ResultCode.BadRequest);
