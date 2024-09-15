85dd24e802c4a088a8a18a99366581353ff061bf
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2024-09-15 17:59:11 +0200

    add: template for signout page

---
+++ src/AuthServer.TestIdentityProvider/Pages/SignOut.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AuthServer.TestIdentityProvider.Pages
+{
+    public class SignOutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
