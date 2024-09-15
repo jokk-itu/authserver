169515a1aab2c26cd5d0cc36e822313f8426fdf5
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2024-09-15 11:55:19 +0200

    add: authorize service for interaction

---
+++ src/AuthServer/Authorize/Abstractions/IAuthorizeService.cs
@@ -0,0 +1,33 @@
+﻿namespace AuthServer.Authorize.Abstractions;
+
+public interface IAuthorizeService
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="subjectIdentifier"></param>
+    /// <param name="clientId"></param>
+    /// <param name="maxAge"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task CreateAuthorizationGrant(string subjectIdentifier, string clientId, long? maxAge, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="subjectIdentifier"></param>
+    /// <param name="clientId"></param>
+    /// <param name="consentGrantDto"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task CreateOrUpdateConsentGrant(string subjectIdentifier, string clientId, ConsentGrantDto consentGrantDto, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="subjectIdentifier"></param>
+    /// <param name="clientId"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task<ConsentGrantDto?> GetConsentGrantDto(string subjectIdentifier, string clientId, CancellationToken cancellationToken);
+}
---
+++ src/AuthServer/Authorize/AuthorizeService.cs
@@ -0,0 +1,45 @@
+﻿using AuthServer.Authorize.Abstractions;
+using AuthServer.Repositories.Abstractions;
+
+namespace AuthServer.Authorize;
+internal class AuthorizeService : IAuthorizeService
+{
+    private readonly IConsentGrantRepository _consentGrantRepository;
+    private readonly IAuthorizationGrantRepository _authorizationGrantRepository;
+
+    public AuthorizeService(
+        IConsentGrantRepository consentGrantRepository,
+        IAuthorizationGrantRepository authorizationGrantRepository)
+    {
+        _consentGrantRepository = consentGrantRepository;
+        _authorizationGrantRepository = authorizationGrantRepository;
+    }
+
+    /// <inheritdoc/>
+    public async Task CreateAuthorizationGrant(string subjectIdentifier, string clientId, long? maxAge,
+        CancellationToken cancellationToken)
+    {
+        await _authorizationGrantRepository.CreateAuthorizationGrant(subjectIdentifier, clientId, maxAge, cancellationToken);
+    }
+
+    /// <inheritdoc/>
+    public async Task CreateOrUpdateConsentGrant(string subjectIdentifier, string clientId, ConsentGrantDto consentGrantDto,
+        CancellationToken cancellationToken)
+    {
+        await _consentGrantRepository.CreateOrUpdateConsentGrant(subjectIdentifier, clientId, consentGrantDto.Scope, consentGrantDto.Claims, cancellationToken);
+    }
+
+    /// <inheritdoc/>
+    public async Task<ConsentGrantDto?> GetConsentGrantDto(string subjectIdentifier, string clientId, CancellationToken cancellationToken)
+    {
+        var consentGrant = await _consentGrantRepository.GetConsentGrant(subjectIdentifier, clientId, cancellationToken);
+        if (consentGrant is null)
+        {
+            return null;
+        }
+
+        return new ConsentGrantDto(
+            consentGrant.ConsentedScopes.Select(x => x.Name),
+            consentGrant.ConsentedClaims.Select(x => x.Name));
+    }
+}
