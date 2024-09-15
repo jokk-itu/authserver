4a5512e67d8818df4f4a0fc62f9d77ca01b85eba
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2024-09-15 11:10:36 +0200

    add: functions for authorizmessage

--- src/AuthServer/Repositories/Abstractions/IClientRepository.cs
+++ src/AuthServer/Repositories/Abstractions/IClientRepository.cs
@@ -1,4 +1,7 @@
-﻿namespace AuthServer.Repositories.Abstractions;
+﻿using AuthServer.Authorize;
+using AuthServer.Entities;
+
+namespace AuthServer.Repositories.Abstractions;
 internal interface IClientRepository
 {
     /// <summary>
@@ -9,4 +12,29 @@ internal interface IClientRepository
     /// <param name="cancellationToken"></param>
     /// <returns></returns>
     Task<bool> DoesResourcesExist(IReadOnlyCollection<string> resources, IReadOnlyCollection<string> scopes, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="reference"></param>
+    /// <param name="clientId"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task<AuthorizeRequestDto?> GetAuthorizeDto(string reference, string clientId, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="authorizeDto"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task<AuthorizeMessage> AddAuthorizeMessage(AuthorizeRequestDto authorizeDto, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="reference"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task RedeemAuthorizeMessage(string reference, CancellationToken cancellationToken);
 }
