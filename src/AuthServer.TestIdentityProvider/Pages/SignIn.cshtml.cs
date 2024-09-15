1434443c4e2fc45e59b013210c9db5004e8a1d73
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2024-09-15 17:58:41 +0200

    add: signin page

---
+++ src/AuthServer.TestIdentityProvider/Pages/SignIn.cshtml.cs
@@ -0,0 +1,67 @@
+using System.Web;
+using AuthServer.Authorize;
+using AuthServer.Authorize.Abstractions;
+using AuthServer.Constants;
+using AuthServer.Core;
+using AuthServer.Tests.Core;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AuthServer.TestIdentityProvider.Pages;
+
+public class SignInModel : PageModel
+{
+    private readonly IAuthorizeUserAccessor _authorizeUserAccessor;
+    private readonly IAuthorizeService _authorizeService;
+
+    public SignInModel(
+        IAuthorizeUserAccessor authorizeUserAccessor,
+        IAuthorizeService authorizeService)
+    {
+        _authorizeUserAccessor = authorizeUserAccessor;
+        _authorizeService = authorizeService;
+    }
+
+    [BindProperty]
+    public InputModel Input { get; set; }
+
+    public string ReturnUrl { get; set; }
+
+    public class InputModel
+    {
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+    }
+
+    public void OnGet(string returnUrl)
+    {
+        ReturnUrl = returnUrl ?? Url.Content("~/");
+    }
+
+    public async Task<IActionResult> OnPost(string returnUrl, CancellationToken cancellationToken)
+    {
+        ReturnUrl = returnUrl ?? Url.Content("~/");
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        if (Input is { Username: UserConstants.Username, Password: UserConstants.Password })
+        {
+            var query = HttpUtility.ParseQueryString(new Uri(ReturnUrl).Query);
+            var hasMaxAge = long.TryParse(query.Get(Parameter.MaxAge), out var parsedMaxAge);
+            await _authorizeService.CreateAuthorizationGrant(UserConstants.SubjectIdentifier, query.Get(Parameter.ClientId)!, hasMaxAge ? parsedMaxAge : null, cancellationToken);
+            _authorizeUserAccessor.SetUser(new AuthorizeUser(UserConstants.SubjectIdentifier, [AmrValueConstants.Password]));
+            var prompt = query.Get(Parameter.Prompt);
+            if (prompt?.Contains(PromptConstants.Consent) == true)
+            {
+                return Redirect($"/Consent?returnUrl={returnUrl}");
+            }
+            
+            return Redirect(ReturnUrl);
+        }
+
+        ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+        return Page();
+    }
+}
