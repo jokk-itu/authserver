02f5371ab5d246bd34bfa6777d73efbede68f6be
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2024-09-15 12:20:24 +0200

    remove: unused properties

--- src/AuthServer/Codes/EncodedAuthorizationCode.cs
+++ src/AuthServer/Codes/EncodedAuthorizationCode.cs
@@ -3,9 +3,7 @@ internal class EncodedAuthorizationCode
 {
     public required string AuthorizationGrantId { get; init; }
     public required string AuthorizationCodeId { get; init; }
-    public required string NonceId { get; init; }
     public required IReadOnlyCollection<string> Scope { get; init; }
     public required string CodeChallenge { get; init; }
-    public required string CodeChallengeMethod { get; init; }
     public string? RedirectUri { get; init; }
 }
--- src/AuthServer/Core/Request/IRequestProcessor.cs
+++ src/AuthServer/Core/Request/IRequestProcessor.cs
@@ -3,5 +3,11 @@ internal interface IRequestProcessor<in TValidatedRequest, TResponse>
     where TValidatedRequest : notnull
 	where TResponse : notnull
 {
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="request"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
 	Task<TResponse> Process(TValidatedRequest request, CancellationToken cancellationToken);
 }
