89ba28350c6ca97dccb5b971b1e5551cf0af13c8
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2024-09-15 12:22:12 +0200

    remove: unused function

--- src/AuthServer/Extensions/DateTimeExtensions.cs
+++ src/AuthServer/Extensions/DateTimeExtensions.cs
@@ -1,11 +1,6 @@
 ﻿namespace AuthServer.Extensions;
 internal static class DateTimeExtensions
 {
-    public static DateTime ToDateTimeUtc(this long unixTimeSeconds)
-    {
-        return DateTime.UnixEpoch.AddSeconds(unixTimeSeconds);
-    }
-
     public static long ToUnixTimeSeconds(this DateTime dateTime)
     {
         return (long)dateTime.Subtract(DateTime.UnixEpoch).TotalSeconds;
