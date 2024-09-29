b9cc778ab0f54d5b25825f0ff3f6eded2fe3928d
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2024-09-29 13:29:35 +0200

    update: endsession page with accept and decline idp logout

--- src/AuthServer.TestIdentityProvider/Pages/SignOut.cshtml.cs
+++ src/AuthServer.TestIdentityProvider/Pages/SignOut.cshtml.cs
@@ -1,12 +1,45 @@
+using AuthServer.EndSession;
+using AuthServer.EndSession.Abstractions;
+using AuthServer.Tests.Core;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace AuthServer.TestIdentityProvider.Pages
+namespace AuthServer.TestIdentityProvider.Pages;
+
+public class SignOutModel : PageModel
 {
-    public class SignOutModel : PageModel
+    private readonly IEndSessionUserAccessor _endSessionUserAccessor;
+
+    public SignOutModel(
+        IEndSessionUserAccessor endSessionUserAccessor)
     {
-        public void OnGet()
-        {
-        }
+        _endSessionUserAccessor = endSessionUserAccessor;
+    }
+
+    public string ReturnUrl { get; set; }
+
+    public void OnGet(string returnUrl)
+    {
+        ReturnUrl = returnUrl ?? Url.Content("~/");
+    }
+
+    public async Task<IActionResult> OnPostAccept(string returnUrl, CancellationToken cancellationToken)
+    {
+        ReturnUrl = returnUrl ?? Url.Content("~/");
+
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        _endSessionUserAccessor.SetUser(new EndSessionUser(UserConstants.SubjectIdentifier, true));
+        return Redirect(ReturnUrl);
+    }
+
+    public async Task<IActionResult> OnPostDecline(string returnUrl, CancellationToken cancellationToken)
+    {
+        ReturnUrl = returnUrl ?? Url.Content("~/");
+
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        _endSessionUserAccessor.SetUser(new EndSessionUser(UserConstants.SubjectIdentifier, false));
+        return Redirect(ReturnUrl);
     }
 }
