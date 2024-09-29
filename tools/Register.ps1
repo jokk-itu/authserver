12c09267357e4e25667d7be90f316cae05b560f5
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2024-09-29 13:27:24 +0200

    update. client tools with uris

--- tools/Register.ps1
+++ tools/Register.ps1
@@ -2,8 +2,10 @@ $body = @{
 	client_name = 'test-client'
 	grant_types = 'authorization_code','refresh_token'
 	scope = 'address openid authserver:userinfo phone email profile'
-	redirect_uris = ,'https://testclient.demo.authserver.dk:7226/signin-oidc'
+	redirect_uris = ,'https://testclient.demo.authserver.dk/signin-oidc'
+	post_logout_redirect_uris = ,'https://testclient.demo.authserver.dk/signout-callback-oidc'
 	token_endpoint_auth_method = 'client_secret_post'
+	backchannel_logout_uri = 'https://testclient.demo.authserver.dk/signout-oidc'
 } | ConvertTo-Json
 
 $response = Invoke-WebRequest `
