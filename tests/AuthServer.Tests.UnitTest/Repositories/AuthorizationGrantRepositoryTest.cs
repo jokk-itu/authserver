c6859f88b973356128c9bc4f99e8690a140f8e05
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2024-10-03 23:24:50 +0200

    add: tests for getting active authorizationgrant

--- tests/AuthServer.Tests.UnitTest/Repositories/AuthorizationGrantRepositoryTest.cs
+++ tests/AuthServer.Tests.UnitTest/Repositories/AuthorizationGrantRepositoryTest.cs
@@ -1,4 +1,5 @@
-﻿using AuthServer.Entities;
+﻿using AuthServer.Constants;
+using AuthServer.Entities;
 using AuthServer.Enums;
 using AuthServer.Repositories.Abstractions;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,7 +14,7 @@ public class AuthorizationGrantRepositoryTest : BaseUnitTest
     }
 
     [Fact]
-    public async Task CreateAuthorizationGrant_ActiveSessionWithMaxAge_ExpectGrant()
+    public async Task CreateAuthorizationGrant_ActiveSessionWithMaxAgeAndPasswordAmr_ExpectGrant()
     {
         // Arrange
         var serviceProvider = BuildServiceProvider();
@@ -28,13 +29,15 @@ public class AuthorizationGrantRepositoryTest : BaseUnitTest
         await AddEntity(client);
 
         // Act
-        var grant = await authorizationGrantRepository.CreateAuthorizationGrant(publicSubjectIdentifier.Id, client.Id, 300, CancellationToken.None);
+        var grant = await authorizationGrantRepository.CreateAuthorizationGrant(publicSubjectIdentifier.Id, client.Id, 300, [AuthenticationMethodReferenceConstants.Password], CancellationToken.None);
 
         // Assert
         Assert.Equal(client, grant.Client);
         Assert.Equal(session, grant.Session);
         Assert.Equal(publicSubjectIdentifier, grant.SubjectIdentifier);
         Assert.Equal(300, grant.MaxAge);
+        Assert.Single(grant.AuthenticationMethodReferences);
+        Assert.Equal(AuthenticationMethodReferenceConstants.Password, grant.AuthenticationMethodReferences.Single().Name);
     }
 
     [Fact]
@@ -54,7 +57,7 @@ public class AuthorizationGrantRepositoryTest : BaseUnitTest
         await AddEntity(previousGrant);
 
         // Act
-        var grant = await authorizationGrantRepository.CreateAuthorizationGrant(publicSubjectIdentifier.Id, client.Id, null, CancellationToken.None);
+        var grant = await authorizationGrantRepository.CreateAuthorizationGrant(publicSubjectIdentifier.Id, client.Id, null, [], CancellationToken.None);
 
         // Assert
         Assert.Equal(client, grant.Client);
@@ -79,7 +82,7 @@ public class AuthorizationGrantRepositoryTest : BaseUnitTest
         await AddEntity(client);
 
         // Act
-        var grant = await authorizationGrantRepository.CreateAuthorizationGrant(publicSubjectIdentifier.Id, client.Id, null, CancellationToken.None);
+        var grant = await authorizationGrantRepository.CreateAuthorizationGrant(publicSubjectIdentifier.Id, client.Id, null, [], CancellationToken.None);
 
         // Assert
         Assert.Equal(client, grant.Client);
@@ -105,7 +108,7 @@ public class AuthorizationGrantRepositoryTest : BaseUnitTest
         await AddEntity(pairwiseSubjectIdentifier);
 
         // Act
-        var grant = await authorizationGrantRepository.CreateAuthorizationGrant(publicSubjectIdentifier.Id, client.Id, null, CancellationToken.None);
+        var grant = await authorizationGrantRepository.CreateAuthorizationGrant(publicSubjectIdentifier.Id, client.Id, null, [], CancellationToken.None);
 
         // Assert
         Assert.Equal(client, grant.Client);
@@ -113,5 +116,133 @@ public class AuthorizationGrantRepositoryTest : BaseUnitTest
         Assert.Equal(publicSubjectIdentifier, grant.Session.PublicSubjectIdentifier);
         Assert.Equal(pairwiseSubjectIdentifier, grant.SubjectIdentifier);
         Assert.Null(grant.MaxAge);
+    }
+
+    [Fact]
+    public async Task GetActiveAuthorizationGrant_SubjectAndClientIdWithRevokedGrant_ExpectNull()
+    {
+        // Arrange
+        var serviceProvider = BuildServiceProvider();
+        var authorizationGrantRepository = serviceProvider.GetRequiredService<IAuthorizationGrantRepository>();
+
+        var publicSubjectIdentifier = new PublicSubjectIdentifier();
+        var session = new Session(publicSubjectIdentifier);
+        var client = new Client("webapp", ApplicationType.Web, TokenEndpointAuthMethod.ClientSecretBasic);
+        var grant = new AuthorizationGrant(session, client, publicSubjectIdentifier);
+        grant.Revoke();
+        await AddEntity(grant);
+
+        // Act
+        var activeGrant = await authorizationGrantRepository.GetActiveAuthorizationGrant(publicSubjectIdentifier.Id, client.Id, CancellationToken.None);
+
+        // Assert
+        Assert.Null(activeGrant);
+    }
+
+    [Fact]
+    public async Task GetActiveAuthorizationGrant_SubjectAndClientIdWithExpiredGrant_ExpectNull()
+    {
+        // Arrange
+        var serviceProvider = BuildServiceProvider();
+        var authorizationGrantRepository = serviceProvider.GetRequiredService<IAuthorizationGrantRepository>();
+
+        var publicSubjectIdentifier = new PublicSubjectIdentifier();
+        var session = new Session(publicSubjectIdentifier);
+        var client = new Client("webapp", ApplicationType.Web, TokenEndpointAuthMethod.ClientSecretBasic);
+        var grant = new AuthorizationGrant(session, client, publicSubjectIdentifier, 0);
+        typeof(AuthorizationGrant).GetProperty("AuthTime")!.SetValue(grant, DateTime.UtcNow.AddSeconds(-60));
+        await AddEntity(grant);
+
+        // Act
+        var activeGrant = await authorizationGrantRepository.GetActiveAuthorizationGrant(publicSubjectIdentifier.Id, client.Id, CancellationToken.None);
+
+        // Assert
+        Assert.Null(activeGrant);
+    }
+
+    [Fact]
+    public async Task GetActiveAuthorizationGrant_SubjectAndClientIdWithRevokedSession_ExpectNull()
+    {
+        // Arrange
+        var serviceProvider = BuildServiceProvider();
+        var authorizationGrantRepository = serviceProvider.GetRequiredService<IAuthorizationGrantRepository>();
+
+        var publicSubjectIdentifier = new PublicSubjectIdentifier();
+        var session = new Session(publicSubjectIdentifier);
+        session.Revoke();
+
+        var client = new Client("webapp", ApplicationType.Web, TokenEndpointAuthMethod.ClientSecretBasic);
+        var grant = new AuthorizationGrant(session, client, publicSubjectIdentifier);
+        await AddEntity(grant);
+
+        // Act
+        var activeGrant = await authorizationGrantRepository.GetActiveAuthorizationGrant(publicSubjectIdentifier.Id, client.Id, CancellationToken.None);
+
+        // Assert
+        Assert.Null(activeGrant);
+    }
+
+    [Fact]
+    public async Task GetActiveAuthorizationGrant_GrantIdWithRevokedGrant_ExpectNull()
+    {
+        // Arrange
+        var serviceProvider = BuildServiceProvider();
+        var authorizationGrantRepository = serviceProvider.GetRequiredService<IAuthorizationGrantRepository>();
+
+        var publicSubjectIdentifier = new PublicSubjectIdentifier();
+        var session = new Session(publicSubjectIdentifier);
+        var client = new Client("webapp", ApplicationType.Web, TokenEndpointAuthMethod.ClientSecretBasic);
+        var grant = new AuthorizationGrant(session, client, publicSubjectIdentifier);
+        grant.Revoke();
+        await AddEntity(grant);
+
+        // Act
+        var activeGrant = await authorizationGrantRepository.GetActiveAuthorizationGrant(grant.Id, CancellationToken.None);
+
+        // Assert
+        Assert.Null(activeGrant);
+    }
+
+    [Fact]
+    public async Task GetActiveAuthorizationGrant_GrantIdWithExpiredGrant_ExpectNull()
+    {
+        // Arrange
+        var serviceProvider = BuildServiceProvider();
+        var authorizationGrantRepository = serviceProvider.GetRequiredService<IAuthorizationGrantRepository>();
+
+        var publicSubjectIdentifier = new PublicSubjectIdentifier();
+        var session = new Session(publicSubjectIdentifier);
+        var client = new Client("webapp", ApplicationType.Web, TokenEndpointAuthMethod.ClientSecretBasic);
+        var grant = new AuthorizationGrant(session, client, publicSubjectIdentifier, 0);
+        typeof(AuthorizationGrant).GetProperty("AuthTime")!.SetValue(grant, DateTime.UtcNow.AddSeconds(-60));
+        await AddEntity(grant);
+
+        // Act
+        var activeGrant = await authorizationGrantRepository.GetActiveAuthorizationGrant(grant.Id, CancellationToken.None);
+
+        // Assert
+        Assert.Null(activeGrant);
+    }
+
+    [Fact]
+    public async Task GetActiveAuthorizationGrant_GrantIdWithRevokedSession_ExpectNull()
+    {
+        // Arrange
+        var serviceProvider = BuildServiceProvider();
+        var authorizationGrantRepository = serviceProvider.GetRequiredService<IAuthorizationGrantRepository>();
+
+        var publicSubjectIdentifier = new PublicSubjectIdentifier();
+        var session = new Session(publicSubjectIdentifier);
+        session.Revoke();
+
+        var client = new Client("webapp", ApplicationType.Web, TokenEndpointAuthMethod.ClientSecretBasic);
+        var grant = new AuthorizationGrant(session, client, publicSubjectIdentifier);
+        await AddEntity(grant);
+
+        // Act
+        var activeGrant = await authorizationGrantRepository.GetActiveAuthorizationGrant(grant.Id, CancellationToken.None);
+
+        // Assert
+        Assert.Null(activeGrant);
     }
 }
