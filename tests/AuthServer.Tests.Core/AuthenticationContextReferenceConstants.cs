1000d11f318f06324d766e98e516826a23a1a5a2
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2024-10-05 16:36:21 +0200

    add: resolver for acr from amr

---
+++ tests/AuthServer.Tests.Core/AuthenticationContextReferenceConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace AuthServer.Tests.Core;
+public static class AuthenticationContextReferenceConstants
+{
+    public const string LevelOfAssuranceLow = "urn:authserver:loa1";
+    public const string LevelOfAssuranceSubstantial = "urn:authserver:loa2";
+    public const string LevelOfAssuranceStrict = "urn:authserver:loa3";
+}
