42dbf2d99b26df7672cfca5bd256af1f9fa27b0a
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2024-10-09 18:04:37 +0200

    update: new inserts for acr

--- tools/Deploy.sql
+++ tools/Deploy.sql
@@ -5,3 +5,27 @@ BEGIN
 		VALUES (NEWID(), 'authserver', 'https://localhost.7254', 0, 0, 0, GETUTCDATE(), 0, 0, 0, 0, 0)
 	END
 END
+
+BEGIN
+	IF NOT EXISTS  (SELECT * FROM AuthenticationContextReference WHERE [Name] = 'urn:authserver:loa1'
+	BEGIN
+		INSERT INTO AuthenticationContextReference ([Name])
+		VALUES ('urn:authserver:loa1')
+	END
+END
+
+BEGIN
+	IF NOT EXISTS  (SELECT * FROM AuthenticationContextReference WHERE [Name] = 'urn:authserver:loa2'
+	BEGIN
+		INSERT INTO AuthenticationContextReference ([Name])
+		VALUES ('urn:authserver:loa2')
+	END
+END
+
+BEGIN
+	IF NOT EXISTS  (SELECT * FROM AuthenticationContextReference WHERE [Name] = 'urn:authserver:loa3'
+	BEGIN
+		INSERT INTO AuthenticationContextReference ([Name])
+		VALUES ('urn:authserver:loa3')
+	END
+END
