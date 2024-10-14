f7fecc2b25f0040a696d8e7d14ffa95b8c509f31
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2024-10-14 12:41:35 +0200

    add: authorize integration tests

--- tests/AuthServer.Tests.IntegrationTest/AuthorizeIntegrationTest.cs
+++ tests/AuthServer.Tests.IntegrationTest/AuthorizeIntegrationTest.cs
@@ -1,12 +1,20 @@
 ﻿using System.Net;
 using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
 using System.Web;
+using AuthServer.Authorize;
+using AuthServer.Authorize.Abstractions;
 using AuthServer.Constants;
 using AuthServer.Core;
 using AuthServer.Endpoints.Responses;
 using AuthServer.Helpers;
+using AuthServer.Tests.Core;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Net.Http.Headers;
 using Xunit.Abstractions;
 
 namespace AuthServer.Tests.IntegrationTest;
@@ -18,34 +26,58 @@ public class AuthorizeIntegrationTest : BaseIntegrationTest
     }
 
     [Fact]
-    public async Task Authorize_NoPrompt_ExpectLoginRequired()
+    public async Task Authorize_NoPrompt_ExpectRedirectCode()
     {
         // Arrange
         var httpClient = GetHttpClient();
-        var registerResponseMessage = await httpClient.PostAsJsonAsync(
-            "connect/register",
-            new Dictionary<string, object>
-            {
-                { Parameter.ClientName, "webapp" },
-                { Parameter.RedirectUris, new[] { "https://webapp.authserver.dk" } },
-            });
-        var registerResponse = (await registerResponseMessage.Content.ReadFromJsonAsync<RegisterResponse>())!;
+        var registerResponse = await GetRegisterResponse(httpClient);
 
-        var proofKeyForCodeExchange = ProofKeyForCodeExchangeHelper.GetProofKeyForCodeExchange();
-        var query = new QueryBuilder
-        {
-            { Parameter.ClientId, registerResponse.ClientId },
-            { Parameter.ResponseMode, ResponseModeConstants.Query },
-            { Parameter.ResponseType, ResponseTypeConstants.Code },
-            { Parameter.CodeChallengeMethod, CodeChallengeMethodConstants.S256 },
-            { Parameter.CodeChallenge, proofKeyForCodeExchange.CodeChallenge },
-            { Parameter.Scope, ScopeConstants.OpenId },
-            { Parameter.State, CryptographyHelper.GetRandomString(32) },
-            { Parameter.Nonce, CryptographyHelper.GetRandomString(32) }
-        }.ToQueryString();
+        await AddUser();
+        await AddAuthenticationContextReferences();
+
+        var authorizeService = ServiceProvider.GetRequiredService<IAuthorizeService>();
+        await authorizeService.CreateAuthorizationGrant(
+            UserConstants.SubjectIdentifier,
+            registerResponse.ClientId,
+            [AuthenticationMethodReferenceConstants.Password],
+            CancellationToken.None);
+
+        await authorizeService.CreateOrUpdateConsentGrant(
+            UserConstants.SubjectIdentifier,
+            registerResponse.ClientId,
+            [ScopeConstants.OpenId],
+            [],
+            CancellationToken.None);
 
         // Act
-        var authorizeResponseMessage = await httpClient.GetAsync($"connect/authorize{query}");
+        var authorizeResponseMessage = await Authorize(httpClient, registerResponse.ClientId, null, true, ScopeConstants.OpenId);
+        var queryNameValues = HttpUtility.ParseQueryString(authorizeResponseMessage.Headers.Location!.Query);
+
+        // Assert
+        Assert.Equal(HttpStatusCode.SeeOther, authorizeResponseMessage.StatusCode);
+        Assert.Equal(registerResponse.RedirectUris!.Single(), authorizeResponseMessage.Headers.Location!.GetLeftPart(UriPartial.Path));
+        Assert.Contains(Parameter.Code, queryNameValues.AllKeys);
+    }
+
+    [Fact]
+    public async Task Authorize_NoPrompt_ExpectRedirectLogin()
+    {
+        // Arrange
+        var httpClient = GetHttpClient();
+        var registerResponse = await GetRegisterResponse(httpClient);
+
+        await AddUser();
+        await AddAuthenticationContextReferences();
+
+        var authorizeService = ServiceProvider.GetRequiredService<IAuthorizeService>();
+        await authorizeService.CreateAuthorizationGrant(
+            UserConstants.SubjectIdentifier,
+            registerResponse.ClientId,
+            [AuthenticationMethodReferenceConstants.Password],
+            CancellationToken.None);
+
+        // Act
+        var authorizeResponseMessage = await Authorize(httpClient, registerResponse.ClientId, 0, true, ScopeConstants.OpenId);
         var queryNameValues = HttpUtility.ParseQueryString(authorizeResponseMessage.Headers.Location!.Query);
         var returnUrl = queryNameValues.Get("returnUrl");
 
@@ -53,5 +85,172 @@ public class AuthorizeIntegrationTest : BaseIntegrationTest
         Assert.Equal(HttpStatusCode.SeeOther, authorizeResponseMessage.StatusCode);
         Assert.Equal(UserInteraction.LoginUri, authorizeResponseMessage.Headers.Location!.GetLeftPart(UriPartial.Path));
         Assert.Equal(authorizeResponseMessage.RequestMessage!.RequestUri!.AbsoluteUri, returnUrl);
+    }
+
+    [Fact]
+    public async Task Authorize_NoPrompt_ExpectRedirectConsent()
+    {
+        // Arrange
+        var httpClient = GetHttpClient();
+        var registerResponse = await GetRegisterResponse(httpClient);
+
+        await AddUser();
+        await AddAuthenticationContextReferences();
+
+        var authorizeService = ServiceProvider.GetRequiredService<IAuthorizeService>();
+        await authorizeService.CreateAuthorizationGrant(
+            UserConstants.SubjectIdentifier,
+            registerResponse.ClientId,
+            [AuthenticationMethodReferenceConstants.Password],
+            CancellationToken.None);
+
+        // Act
+        var authorizeResponseMessage = await Authorize(httpClient, registerResponse.ClientId, null, true, ScopeConstants.OpenId);
+        var queryNameValues = HttpUtility.ParseQueryString(authorizeResponseMessage.Headers.Location!.Query);
+        var returnUrl = queryNameValues.Get("returnUrl");
+
+        // Assert
+        Assert.Equal(HttpStatusCode.SeeOther, authorizeResponseMessage.StatusCode);
+        Assert.Equal(UserInteraction.ConsentUri, authorizeResponseMessage.Headers.Location!.GetLeftPart(UriPartial.Path));
+        Assert.Equal(authorizeResponseMessage.RequestMessage!.RequestUri!.AbsoluteUri, returnUrl);
+    }
+
+    [Fact]
+    public async Task Authorize_NoPrompt_ExpectRedirectSelectAccount()
+    {
+        // Arrange
+        var httpClient = GetHttpClient();
+        var registerResponse = await GetRegisterResponse(httpClient);
+
+        await AddUser();
+        await AddAuthenticationContextReferences();
+
+        var authorizeService = ServiceProvider.GetRequiredService<IAuthorizeService>();
+        await authorizeService.CreateAuthorizationGrant(
+            UserConstants.SubjectIdentifier,
+            registerResponse.ClientId,
+            [AuthenticationMethodReferenceConstants.Password],
+            CancellationToken.None);
+
+        // Act
+        var authorizeResponseMessage = await Authorize(httpClient, registerResponse.ClientId, null, false, ScopeConstants.OpenId);
+        var queryNameValues = HttpUtility.ParseQueryString(authorizeResponseMessage.Headers.Location!.Query);
+        var returnUrl = queryNameValues.Get("returnUrl");
+
+        // Assert
+        Assert.Equal(HttpStatusCode.SeeOther, authorizeResponseMessage.StatusCode);
+        Assert.Equal(UserInteraction.AccountSelectionUri, authorizeResponseMessage.Headers.Location!.GetLeftPart(UriPartial.Path));
+        Assert.Equal(authorizeResponseMessage.RequestMessage!.RequestUri!.AbsoluteUri, returnUrl);
+    }
+
+    [Fact]
+    public async Task Authorize_InvalidClientId_ExpectBadRequest()
+    {
+        // Arrange
+        var httpClient = GetHttpClient();
+
+        // Act
+        var authorizeResponseMessage = await Authorize(httpClient, "invalid_client_id", null, false, ScopeConstants.OpenId);
+        var content = await authorizeResponseMessage.Content.ReadAsStringAsync();
+        var error = JsonSerializer.Deserialize<OAuthError>(content);
+
+        // Assert
+        Assert.Equal(HttpStatusCode.BadRequest, authorizeResponseMessage.StatusCode);
+        Assert.NotNull(error);
+        Assert.Equal(ErrorCode.InvalidClient, error.Error);
+        Assert.NotNull(error.ErrorDescription);
+    }
+
+    [Fact]
+    public async Task Authorize_InvalidScope_ExpectRedirectInvalidScope()
+    {
+        // Arrange
+        var httpClient = GetHttpClient();
+        var registerResponse = await GetRegisterResponse(httpClient);
+
+        await AddUser();
+        await AddAuthenticationContextReferences();
+
+        var authorizeService = ServiceProvider.GetRequiredService<IAuthorizeService>();
+        await authorizeService.CreateAuthorizationGrant(
+            UserConstants.SubjectIdentifier,
+            registerResponse.ClientId,
+            [AuthenticationMethodReferenceConstants.Password],
+            CancellationToken.None);
+
+        // Act
+        var authorizeResponseMessage = await Authorize(httpClient, registerResponse.ClientId, 0, true, "invalid_scope");
+        var queryNameValues = HttpUtility.ParseQueryString(authorizeResponseMessage.Headers.Location!.Query);
+        var error = queryNameValues.Get(Parameter.Error);
+        var errorDescription = queryNameValues.Get(Parameter.ErrorDescription);
+
+        // Assert
+        Assert.Equal(HttpStatusCode.SeeOther, authorizeResponseMessage.StatusCode);
+        Assert.Equal(registerResponse.RedirectUris!.Single(), authorizeResponseMessage.Headers.Location!.GetLeftPart(UriPartial.Path));
+        Assert.Equal(ErrorCode.InvalidScope, error);
+        Assert.NotNull(errorDescription);
+    }
+
+    private async Task<RegisterResponse> GetRegisterResponse(HttpClient httpClient)
+    {
+        var registerResponseMessage = await httpClient.PostAsJsonAsync(
+            "connect/register",
+            new Dictionary<string, object>
+            {
+                { Parameter.ClientName, "webapp" },
+                { Parameter.RedirectUris, new[] { "https://webapp.authserver.dk/" } },
+            });
+        var registerResponse = (await registerResponseMessage.Content.ReadFromJsonAsync<RegisterResponse>())!;
+
+        TestOutputHelper.WriteLine(
+            "Received Register response {0}, Content: {1}",
+            registerResponseMessage.StatusCode,
+            await registerResponseMessage.Content.ReadAsStringAsync());
+
+        return registerResponse;
+    }
+
+    private async Task<HttpResponseMessage> Authorize(HttpClient httpClient, string clientId, int? maxAge, bool appendAuthorizeUser, string scope)
+    {
+        var proofKeyForCodeExchange = ProofKeyForCodeExchangeHelper.GetProofKeyForCodeExchange();
+        var queryBuilder = new QueryBuilder
+        {
+            { Parameter.ClientId, clientId },
+            { Parameter.ResponseType, ResponseTypeConstants.Code },
+            { Parameter.CodeChallengeMethod, CodeChallengeMethodConstants.S256 },
+            { Parameter.CodeChallenge, proofKeyForCodeExchange.CodeChallenge },
+            { Parameter.Scope, scope },
+            { Parameter.State, CryptographyHelper.GetRandomString(32) },
+            { Parameter.Nonce, CryptographyHelper.GetRandomString(32) },
+        };
+
+        if (maxAge is not null)
+        {
+            queryBuilder.Add(Parameter.MaxAge, maxAge.ToString()!);
+        }
+
+        var query = queryBuilder.ToQueryString();
+        var requestMessage = new HttpRequestMessage(HttpMethod.Get, $"connect/authorize{query}");
+
+        var dataProtectorProvider = ServiceProvider.GetRequiredService<IDataProtectionProvider>();
+        var dataProtector = dataProtectorProvider.CreateProtector(AuthorizeUserAccessor.DataProtectorPurpose);
+        var authorizeUser = new AuthorizeUser(UserConstants.SubjectIdentifier);
+        var authorizeUserBytes = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(authorizeUser));
+        var encryptedAuthorizeUser = dataProtector.Protect(authorizeUserBytes);
+        var cookieValue = Convert.ToBase64String(encryptedAuthorizeUser);
+
+        if (appendAuthorizeUser)
+        {
+            requestMessage.Headers.Add("Cookie", new CookieHeaderValue("AuthorizeUser", cookieValue).ToString());
+        }
+
+        var authorizeResponseMessage = await httpClient.SendAsync(requestMessage);
+
+        TestOutputHelper.WriteLine("Received Authorize response {0}, Location: {1}, Content: {2}",
+            authorizeResponseMessage.StatusCode,
+            authorizeResponseMessage.Headers.Location,
+            await authorizeResponseMessage.Content.ReadAsStringAsync());
+
+        return authorizeResponseMessage;
     }
 }
--- tests/AuthServer.Tests.IntegrationTest/BaseIntegrationTest.cs
+++ tests/AuthServer.Tests.IntegrationTest/BaseIntegrationTest.cs
@@ -1,13 +1,17 @@
-﻿using AuthServer.Core;
+﻿using AuthServer.Constants;
+using AuthServer.Core;
+using AuthServer.Core.Abstractions;
 using AuthServer.Core.Discovery;
 using AuthServer.Entities;
 using AuthServer.Enums;
 using AuthServer.Options;
+using AuthServer.Tests.Core;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
+using Moq;
 using Xunit.Abstractions;
 
 namespace AuthServer.Tests.IntegrationTest;
@@ -29,9 +33,29 @@ public abstract class BaseIntegrationTest : IClassFixture<WebApplicationFactory<
     private readonly IOptionsMonitor<JwksDocument> _jwksDocumentOptions;
     protected JwksDocument JwksDocument => _jwksDocumentOptions.CurrentValue;
 
+    protected const string LevelOfAssuranceLow = AuthenticationContextReferenceConstants.LevelOfAssuranceLow;
+
+    protected const string LevelOfAssuranceSubstantial =
+        AuthenticationContextReferenceConstants.LevelOfAssuranceSubstantial;
+
+    protected const string LevelOfAssuranceStrict = AuthenticationContextReferenceConstants.LevelOfAssuranceStrict;
+
     protected BaseIntegrationTest(WebApplicationFactory<Program> factory, ITestOutputHelper testOutputHelper)
     {
-        _factory = factory.WithWebHostBuilder(builder => { builder.UseEnvironment("Integration"); });
+        _factory = factory.WithWebHostBuilder(builder =>
+        {
+            builder.UseEnvironment("Integration");
+            builder.ConfigureServices(services =>
+            {
+                var authenticatedUserAccessor = new Mock<IAuthenticatedUserAccessor>();
+                authenticatedUserAccessor
+                    .Setup(x => x.CountAuthenticatedUsers())
+                    .ReturnsAsync(2);
+
+                services.AddScopedMock(authenticatedUserAccessor);
+            });
+        });
+
         TestOutputHelper = testOutputHelper;
         ServiceProvider = _factory.Services.CreateScope().ServiceProvider;
 
@@ -55,7 +79,7 @@ public abstract class BaseIntegrationTest : IClassFixture<WebApplicationFactory<
         const string scopeName = "weather:read";
         var scope = new Scope(scopeName);
 
-        dbContext.Add(scope);
+        await dbContext.AddAsync(scope);
         await dbContext.SaveChangesAsync();
 
         return scopeName;
@@ -68,12 +92,59 @@ public abstract class BaseIntegrationTest : IClassFixture<WebApplicationFactory<
         var weatherScope = await dbContext.Set<Scope>().SingleAsync(x => x.Name == "weather:read");
         var client = new Client("weather-api", ApplicationType.Web, TokenEndpointAuthMethod.ClientSecretBasic)
         {
-            Scopes = [ weatherScope ],
+            Scopes = [weatherScope],
             ClientUri = "https://weather.authserver.dk"
         };
-        dbContext.Add(client);
+
+        await dbContext.AddAsync(client);
         await dbContext.SaveChangesAsync();
 
         return client;
+    }
+
+    protected async Task<Client> AddIdentityProviderClient()
+    {
+        var dbContext = ServiceProvider.GetRequiredService<AuthorizationDbContext>();
+
+        var userinfoScope = await dbContext.Set<Scope>().SingleAsync(x => x.Name == ScopeConstants.UserInfo);
+        var client = new Client("identity-provider", ApplicationType.Web, TokenEndpointAuthMethod.ClientSecretBasic)
+        {
+            Scopes = [userinfoScope],
+            ClientUri = "https://idp.authserver.dk"
+        };
+
+        await dbContext.AddAsync(client);
+        await dbContext.SaveChangesAsync();
+
+        return client;
+    }
+
+    protected async Task AddUser()
+    {
+        var dbContext = ServiceProvider.GetRequiredService<AuthorizationDbContext>();
+
+        var publicSubjectIdentifier = new PublicSubjectIdentifier();
+        typeof(PublicSubjectIdentifier)
+            .GetProperty(nameof(PublicSubjectIdentifier.Id))!
+            .SetValue(publicSubjectIdentifier, UserConstants.SubjectIdentifier);
+
+        await dbContext.AddAsync(publicSubjectIdentifier);
+        await dbContext.SaveChangesAsync();
+    }
+
+    protected async Task AddAuthenticationContextReferences()
+    {
+        var dbContext = ServiceProvider.GetRequiredService<AuthorizationDbContext>();
+
+        var authenticationContextReferenceLow = new AuthenticationContextReference(LevelOfAssuranceLow);
+        var authenticationContextReferenceSubstantial = new AuthenticationContextReference(LevelOfAssuranceSubstantial);
+        var authenticationContextReferenceStrict = new AuthenticationContextReference(LevelOfAssuranceStrict);
+
+        dbContext.AddRange(
+            authenticationContextReferenceLow,
+            authenticationContextReferenceSubstantial,
+            authenticationContextReferenceStrict);
+
+        await dbContext.SaveChangesAsync();
     }
 }
