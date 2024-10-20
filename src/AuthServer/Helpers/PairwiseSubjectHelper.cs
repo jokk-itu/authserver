5efa735b4ea08ca6a1acd51d6f43ee4244a69aaa
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2024-10-20 13:33:09 +0200

    add: helper to generate pairwise subjects

---
+++ src/AuthServer/Helpers/PairwiseSubjectHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+using AuthServer.Entities;
+
+namespace AuthServer.Helpers;
+
+public static class PairwiseSubjectHelper
+{
+    public static string GenerateSubject(SectorIdentifier sectorIdentifier, string subjectIdentifier)
+    {
+        var host = sectorIdentifier!.GetHostComponent();
+        var salt = sectorIdentifier!.Salt;
+        return new StringBuilder()
+            .Append(subjectIdentifier)
+            .Append(host)
+            .Append(salt)
+            .ToString()
+            .Sha256();
+    }
+}
