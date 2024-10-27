72949f39dbfcd4027aad6b33f39e417b9b5e0aaa
jokk-itu <71125597+jokk-itu@users.noreply.github.com>
2024-10-27 16:55:47 +0000

    Deploying to gh-pages from @ jokk-itu/authserver@83e6f8f95997aff3698bb745e0aa3ec59e56477c 🚀

--- _app/immutable/nodes/18.326242f1.js
+++
@@ -1,5 +0,0 @@
-import{S as d,i as g,s as h,y as c,a as x,z as m,c as I,A as p,b as l,g as u,d as f,B as $,h as _,q as w,r as A}from"../chunks/index.8cffed02.js";import{P}from"../chunks/PageTitle.df959b80.js";import{S as q}from"../chunks/Section.be805b22.js";function y(o){let t;return{c(){t=w(`An API functioning as a protected resource.
-It exposes data about the weather randomly.
-It requires the weather:read scope.`)},l(a){t=A(a,`An API functioning as a protected resource.
-It exposes data about the weather randomly.
-It requires the weather:read scope.`)},m(a,n){l(a,t,n)},d(a){a&&_(t)}}}function S(o){let t,a,n,s;return t=new P({props:{title:"Weather"}}),n=new q({props:{title:"Introduction",$$slots:{default:[y]},$$scope:{ctx:o}}}),{c(){c(t.$$.fragment),a=x(),c(n.$$.fragment)},l(e){m(t.$$.fragment,e),a=I(e),m(n.$$.fragment,e)},m(e,r){p(t,e,r),l(e,a,r),p(n,e,r),s=!0},p(e,[r]){const i={};r&1&&(i.$$scope={dirty:r,ctx:e}),n.$set(i)},i(e){s||(u(t.$$.fragment,e),u(n.$$.fragment,e),s=!0)},o(e){f(t.$$.fragment,e),f(n.$$.fragment,e),s=!1},d(e){$(t,e),e&&_(a),$(n,e)}}}class B extends d{constructor(t){super(),g(this,t,null,S,h,{})}}export{B as component};
--- _app/immutable/nodes/6.5f6bae20.js
+++
@@ -1,9 +0,0 @@
-import{S as _,i as A,s as h,y as c,a as b,z as m,c as g,A as p,b as d,g as f,d as u,B as $,h as r,k as F,q as B,l as P,m as y,r as x,D as I,H as z}from"../chunks/index.8cffed02.js";import{P as C}from"../chunks/PageTitle.df959b80.js";import{S as v}from"../chunks/Section.be805b22.js";function S(i){let t,s;return{c(){t=F("p"),s=B(`A client built using Blazor WebAssembly.
-It contains of a Client compiled into WebAssembly and is the Frontend in BFF.
-An API responsible for Authentication and Authorization,
-and is the Backend in BFF.
-The API also functions as a Reverse Proxy for the Client.`)},l(n){t=P(n,"P",{});var a=y(t);s=x(a,`A client built using Blazor WebAssembly.
-It contains of a Client compiled into WebAssembly and is the Frontend in BFF.
-An API responsible for Authentication and Authorization,
-and is the Backend in BFF.
-The API also functions as a Reverse Proxy for the Client.`),a.forEach(r)},m(n,a){d(n,t,a),I(t,s)},p:z,d(n){n&&r(t)}}}function W(i){let t,s,n,a;return t=new C({props:{title:"Blazor BFF"}}),n=new v({props:{title:"Introduction",$$slots:{default:[S]},$$scope:{ctx:i}}}),{c(){c(t.$$.fragment),s=b(),c(n.$$.fragment)},l(e){m(t.$$.fragment,e),s=g(e),m(n.$$.fragment,e)},m(e,o){p(t,e,o),d(e,s,o),p(n,e,o),a=!0},p(e,[o]){const l={};o&1&&(l.$$scope={dirty:o,ctx:e}),n.$set(l)},i(e){a||(f(t.$$.fragment,e),f(n.$$.fragment,e),a=!0)},o(e){u(t.$$.fragment,e),u(n.$$.fragment,e),a=!1},d(e){$(t,e),e&&r(s),$(n,e)}}}class w extends _{constructor(t){super(),A(this,t,null,W,h,{})}}export{w as component};
