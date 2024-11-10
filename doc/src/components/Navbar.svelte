d31268dbbb215081039ab9a41c2f9c003d2e8d3f
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2024-11-10 16:14:23 +0100

    add: developer page

--- doc/src/components/Navbar.svelte
+++ doc/src/components/Navbar.svelte
@@ -23,18 +23,20 @@
               d="M14.54 .887L10.91 2.66V20.83C6.76 20.31 3.64 18.05 3.64 15.33C3.64 12.75 6.44 10.58 10.27 9.92V7.61C4.42 8.32 0 11.5 0 15.33C0 19.29 4.74 22.57 10.91 23.11L14.54 21.4V.886M15.18 7.61V9.92C16.61 10.17 17.89 10.62 18.94 11.23L16.97 12.34L24 13.87L23.5 8.66L21.63 9.72C19.89 8.66 17.67 7.91 15.18 7.61Z"
             />
           </svg>
-          <span class="font-bold">AuthServer</span>
+          <span class="font-bold hidden md:block">AuthServer</span>
         </a>
       </div>
 
       <!-- primary nav -->
       <div class="flex items-center space-x-1">
-        <a href="{base}/introduction" class="py-5 px-3 text-gray-700 hover:text-gray-900"
-          >Introduction</a
+        <a href="{base}/intro" class="py-5 px-3 text-gray-700 hover:text-gray-900"
+          >Intro</a
         >
         <a href="{base}/demo" class="py-5 px-3 text-gray-700 hover:text-gray-900"
           >Demo</a
         >
+        <a href="{base}/developer" class="py-5 px-3 text-gray-700 hover:text-gray-900"
+          >Developer</a>
       </div>
 
       <!-- secondary nav -->
---
+++ doc/src/routes/developer/+page.svelte
@@ -0,0 +1,43 @@
+<h1 class="text-4xl mb-2">Developer Documentation</h1>
+<p class="mb-4">
+    Describes how to implement your own custom IdentityProvider using AuthServer.
+    <br/>
+    How AuthServer functions and its architectural principles.
+    <br/>
+    The features supported by AuthServer, with integration design.
+</p>
+
+<h2 class="text-xl mb-4">Custom IdentityProvider</h2>
+<p>TODO</p>
+<br/>
+
+<h2 class="text-xl mb-4">Architecture</h2>
+<br/>
+<h3 class="text-lg mb-4">Request architecture</h3>
+<p>Component diagram</p>
+<br/>
+
+<h3 class="text-lg mb-4">Client caching</h3>
+<p>
+    Client data is fetched and utilized at almost every endpoint, and clients are rarely updated.
+    Therefore, clients are stored in a configurable distributed cache, to allow for fast retrieval.
+    If a client is updated, then upon commit, the cache for that client is flushed.
+</p>
+<br/>
+
+<h3 class="text-lg mb-4">Telemetry</h3>
+<p>The Open Telemetry standard is used to expose data on the system.</p>
+<br/>
+
+<h3 class="text-lg mb-4">Datamodel</h3>
+<p>ER diagram</p>
+<br/>
+
+<h2 class="text-xl mb-4">Authorization Code grant</h2>
+<p>Introduction</p>
+<p>Authorize request</p>
+<p>Authorize response</p>
+<p>Token request</p>
+<p>Token response</p>
+
+
