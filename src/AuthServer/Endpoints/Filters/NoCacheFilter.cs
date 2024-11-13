fb7681ba38fe33896f45b66f02429db9bd0b2fee
NCLAN\jkke <jkke@netcompany.com>
2024-11-13 21:48:03 +0100

    add: endpoint filters for response headers

---
+++ src/AuthServer/Endpoints/Filters/NoCacheFilter.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace AuthServer.Endpoints.Filters;
+
+internal class NoCacheFilter : IEndpointFilter
+{
+    public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
+    {
+        context.HttpContext.Response.Headers.CacheControl = "no-cache, no-store";
+        return await next(context);
+    }
+}
---
+++ src/AuthServer/Endpoints/Filters/NoReferrerFilter.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+
+namespace AuthServer.Endpoints.Filters;
+
+internal class NoReferrerFilter : IEndpointFilter
+{
+    public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
+    {
+        context.HttpContext.Response.Headers.Add(new KeyValuePair<string, StringValues>("Referrer-Policy", "no-referrer"));
+        return await next(context);
+    }
+}
