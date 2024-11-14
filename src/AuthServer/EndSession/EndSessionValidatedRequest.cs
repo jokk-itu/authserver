405b36a6ecf23513e0ec217c1096a9b6966a0b64
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2024-11-14 19:12:16 +0100

    update: do not make required as it is nullable or primitive

--- src/AuthServer/EndSession/EndSessionValidatedRequest.cs
+++ src/AuthServer/EndSession/EndSessionValidatedRequest.cs
@@ -1,8 +1,8 @@
 ﻿namespace AuthServer.EndSession;
 internal class EndSessionValidatedRequest
 {
-    public required string? SubjectIdentifier { get; init; }
-    public required string? SessionId { get; init; }
-    public required string? ClientId { get; init; }
-    public required bool LogoutAtIdentityProvider { get; init; }
+    public string? SubjectIdentifier { get; init; }
+    public string? SessionId { get; init; }
+    public string? ClientId { get; init; }
+    public bool LogoutAtIdentityProvider { get; init; }
 }
