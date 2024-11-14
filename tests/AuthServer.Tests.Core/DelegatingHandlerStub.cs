030fec6fee8bb83b547d18fe897fb44175819b44
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2024-11-14 17:01:21 +0100

    update: constructor for deletegate stub to only accept status code

--- tests/AuthServer.Tests.Core/DelegatingHandlerStub.cs
+++ tests/AuthServer.Tests.Core/DelegatingHandlerStub.cs
@@ -5,12 +5,18 @@ namespace AuthServer.Tests.Core;
 public class DelegatingHandlerStub : DelegatingHandler
 {
     private readonly Func<HttpRequestMessage, CancellationToken, Task<HttpResponseMessage>> _handlerFunc;
+
     public DelegatingHandlerStub(string content, string contentType, HttpStatusCode statusCode)
     {
         _handlerFunc = (_, _) => Task.FromResult(new HttpResponseMessage(statusCode)
         {
             Content = new StringContent(content, Encoding.UTF8, contentType)
         });
+    }
+
+    public DelegatingHandlerStub(HttpStatusCode httpStatusCode)
+    {
+        _handlerFunc = (_, _) => Task.FromResult(new HttpResponseMessage(httpStatusCode));
     }
 
     public DelegatingHandlerStub(Func<HttpRequestMessage, CancellationToken, Task<HttpResponseMessage>> handlerFunc)
