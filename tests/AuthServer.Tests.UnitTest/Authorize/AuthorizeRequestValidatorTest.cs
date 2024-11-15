475f03f22dff50e6e980a037064a60f799ded143
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2024-11-15 22:00:42 +0100

    add: initial tests for authorize request validator

--- tests/AuthServer.Tests.UnitTest/Authorize/AuthorizeRequestValidatorTest.cs
+++ tests/AuthServer.Tests.UnitTest/Authorize/AuthorizeRequestValidatorTest.cs
@@ -1,9 +1,14 @@
-﻿using AuthServer.Authorize;
+﻿using AuthServer.Authorization.Abstractions;
+using AuthServer.Authorize;
+using AuthServer.Authorize.Abstractions;
+using AuthServer.Constants;
 using AuthServer.Core.Abstractions;
 using AuthServer.Entities;
 using AuthServer.Enums;
 using AuthServer.RequestAccessors.Authorize;
+using AuthServer.Tests.Core;
 using Microsoft.Extensions.DependencyInjection;
+using Moq;
 using Xunit.Abstractions;
 
 namespace AuthServer.Tests.UnitTest.Authorize;
@@ -60,5 +65,111 @@ public class AuthorizeRequestValidatorTest : BaseUnitTest
 
         // Assert
         Assert.Equal(AuthorizeError.InvalidRequestAndRequestUri, processResult);
+    }
+
+    [Fact]
+    public async Task Validate_RequireSignedRequestWithEmptyRequestAndRequestUri_ExpectRequestOrRequestUriRequiredAsRequestObject()
+    {
+        // Arrange
+        var serviceProvider = BuildServiceProvider();
+        var validator = serviceProvider.GetRequiredService<
+            IRequestValidator<AuthorizeRequest, AuthorizeValidatedRequest>>();
+
+        var client = new Client("web-app", ApplicationType.Web, TokenEndpointAuthMethod.ClientSecretBasic)
+        {
+            RequireSignedRequestObject = true
+        };
+        await AddEntity(client);
+
+        var request = new AuthorizeRequest
+        {
+            ClientId = client.Id
+        };
+
+        // Act
+        var processResult = await validator.Validate(request, CancellationToken.None);
+
+        // Assert
+        Assert.Equal(AuthorizeError.RequestOrRequestUriRequiredAsRequestObject, processResult);
+    }
+
+    [Fact]
+    public async Task Validate_RequirePushedAuthorizationWithEmptyRequestUri_ExpectRequestUriRequiredAsPushedAuthorizationRequest()
+    {
+        // Arrange
+        var serviceProvider = BuildServiceProvider();
+        var validator = serviceProvider.GetRequiredService<
+            IRequestValidator<AuthorizeRequest, AuthorizeValidatedRequest>>();
+
+        var client = new Client("web-app", ApplicationType.Web, TokenEndpointAuthMethod.ClientSecretBasic)
+        {
+            RequirePushedAuthorizationRequests = true
+        };
+        await AddEntity(client);
+
+        var request = new AuthorizeRequest
+        {
+            ClientId = client.Id
+        };
+
+        // Act
+        var processResult = await validator.Validate(request, CancellationToken.None);
+
+        // Assert
+        Assert.Equal(AuthorizeError.RequestUriRequiredAsPushedAuthorizationRequest, processResult);
+    }
+
+    [Fact]
+    public async Task Validate_InvalidRequestUriFromPushedAuthorization_ExpectInvalidOrExpireRequestUri()
+    {
+        // Arrange
+        var serviceProvider = BuildServiceProvider();
+        var validator = serviceProvider.GetRequiredService<
+            IRequestValidator<AuthorizeRequest, AuthorizeValidatedRequest>>();
+
+        var client = new Client("web-app", ApplicationType.Web, TokenEndpointAuthMethod.ClientSecretBasic);
+        await AddEntity(client);
+
+        var request = new AuthorizeRequest
+        {
+            ClientId = client.Id,
+            RequestUri = $"{RequestUriConstants.RequestUriPrefix}invalid_value"
+        };
+
+        // Act
+        var processResult = await validator.Validate(request, CancellationToken.None);
+
+        // Assert
+        Assert.Equal(AuthorizeError.InvalidOrExpiredRequestUri, processResult);
+    }
+
+    [Fact]
+    public async Task Validate_ValidRequestUriFromPushedAuthorization_ExpectAuthorizeValidatedRequest()
+    {
+        // Arrange
+        var secureRequestService = new Mock<ISecureRequestService>();
+        var authorizeInteractionService = new Mock<IAuthorizeInteractionService>();
+        var serviceProvider = BuildServiceProvider(services =>
+        {
+            services.AddScopedMock(secureRequestService);
+            services.AddScopedMock(authorizeInteractionService);
+        });
+        var validator = serviceProvider.GetRequiredService<
+            IRequestValidator<AuthorizeRequest, AuthorizeValidatedRequest>>();
+
+        var client = new Client("web-app", ApplicationType.Web, TokenEndpointAuthMethod.ClientSecretBasic);
+        await AddEntity(client);
+
+        var request = new AuthorizeRequest
+        {
+            ClientId = client.Id,
+            RequestUri = $"{RequestUriConstants.RequestUriPrefix}invalid_value"
+        };
+
+        // Act
+        var processResult = await validator.Validate(request, CancellationToken.None);
+
+        // Assert
+        Assert.Equal(AuthorizeError.InvalidOrExpiredRequestUri, processResult);
     }
 }
